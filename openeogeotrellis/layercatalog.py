dbefa9b19665be321ee28f5a53e3c5f5bb5ff64e
Jeroen Dries <jeroen.dries@vito.be>
2018-06-13 15:06:33 +0200

    updated layercatalog to follow OpenEO API 0.3.0

--- openeogeotrellis/layercatalog.py
+++ openeogeotrellis/layercatalog.py
@@ -2,19 +2,24 @@ import unittest
 from typing import List
 
 import geopyspark as gps
-
+import json
+import os
 
 class LayerCatalog():
+
     """Catalog providing access to GeoPySpark layers"""
     def __init__(self):
-        pass
-
+        if os.path.isfile("layercatalog.json"):
+            with open("layercatalog.json","r+") as f:
+                self.catalog = {layer["data_id"]:layer for layer in json.load(f)}
+        else:
+            raise RuntimeError("layercatalog.json not found, please make sure that it is available in the working directory.")
 
     def layers(self) -> List:
         """Returns all available layers."""
         #TODO make this work with Kerberos authentication
         store = gps.AttributeStore("accumulo+kerberos://epod6.vgt.vito.be:2181/hdp-accumulo-instance")
         layers = store.layers()
-        return [{"product_id":layer.layer_name ,"zoom":layer.layer_zoom} for layer in layers]
+        return [self.catalog[layer.layer_name] for layer in layers if layer.layer_name in self.catalog and layer.layer_zoom ==0]
 
 
