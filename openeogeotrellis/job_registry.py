3d245c274af9f9b2f5e151fcec0b72bf2882096a
Jan Van den bosch <jan@bossie.org>
2018-09-06 15:28:11 +0200

    EP-2651: added JobRegistry class

---
+++ openeogeotrellis/job_registry.py
@@ -0,0 +1,12 @@
+class JobRegistry:
+    def __init__(self):
+        pass
+
+    def add_job(self, job_id: str) -> None:
+        pass
+
+    def set_application_id(self, job_id: str, application_id: str) -> None:
+        pass
+
+    def set_status(self, job_id: str, status: str) -> None:
+        pass
