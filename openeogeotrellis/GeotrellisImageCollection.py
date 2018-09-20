ee6a37fcbc7bcfb445193853f69ab65a133bf2a3
Jan Van den bosch <jan@bossie.org>
2018-09-20 11:23:33 +0200

    EP-2651: support COG output

--- openeogeotrellis/GeotrellisImageCollection.py
+++ openeogeotrellis/GeotrellisImageCollection.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Union, Iterable, Tuple
 
 import geopyspark as gps
 from datetime import datetime, date
-from geopyspark import TiledRasterLayer, TMS, Pyramid,Tile,SpaceTimeKey,Metadata
+from geopyspark import TiledRasterLayer, TMS, Pyramid,Tile,SpaceTimeKey,Metadata,Extent
 from geopyspark.geotrellis.constants import CellType
 from geopyspark.geotrellis import color
 import numpy as np
@@ -247,9 +247,70 @@ class GeotrellisTimeSeriesImageCollection(ImageCollection):
         spatial_rdd = self.pyramid.levels[self.pyramid.max_zoom]
         if spatial_rdd.layer_type != gps.LayerType.SPATIAL:
             spatial_rdd = spatial_rdd.to_spatial_layer()
-        spatial_rdd.save_stitched(filename)
+
+        tiled = (format_options.get("format", "GTiff") == "GTiff" and
+                 format_options.get("parameters", {}).get("tiled", False))
+
+        if tiled:
+            self.save_stitched_tiled(spatial_rdd, filename)
+        else:
+            spatial_rdd.save_stitched(filename)
 
         return filename
+
+    def save_stitched_tiled(self, spatial_rdd, filename):
+        import rasterio as rstr
+        from affine import Affine
+        import rasterio._warp as rwarp
+        from math import log
+
+        max_level = self.pyramid.levels[self.pyramid.max_zoom]
+
+        spatial_rdd = spatial_rdd.persist()
+
+        sorted_keys = sorted(spatial_rdd.collect_keys())
+
+        upper_left_coords = GeotrellisTimeSeriesImageCollection._mapTransform(max_level.layer_metadata.layout_definition, sorted_keys[0])
+        lower_right_coords = GeotrellisTimeSeriesImageCollection._mapTransform(max_level.layer_metadata.layout_definition, sorted_keys[-1])
+
+        data = spatial_rdd.stitch()
+
+        bands, w, h = data.cells.shape
+        nodata = max_level.layer_metadata.no_data_value
+        dtype = data.cells.dtype
+        ex = Extent(xmin=upper_left_coords.left, ymin=lower_right_coords.bottom, xmax=lower_right_coords.right, ymax=upper_left_coords.top)
+        cw, ch = (ex.xmax - ex.xmin) / w, (ex.ymax - ex.ymin) / h
+        overview_level = int(log(w) / log(2) - 8)
+
+        with rstr.io.MemoryFile() as memfile, open(filename, 'wb') as f:
+            with memfile.open(driver='GTiff',
+                              count=bands,
+                              width=w,
+                              height=h,
+                              transform=Affine(cw, 0.0, ex.xmin,
+                                               0.0, -ch, ex.ymax),
+                              crs=rstr.crs.CRS.from_epsg(4326),
+                              nodata=nodata,
+                              dtype=dtype,
+                              compress='lzw',
+                              tiled=True) as mem:
+                windows = list(mem.block_windows(1))
+                for _, w in windows:
+                    segment = data.cells[:, w.row_off:(w.row_off + w.height), w.col_off:(w.col_off + w.width)]
+                    mem.write(segment, window=w)
+                    mask_value = np.all(segment != nodata, axis=0).astype(np.uint8) * 255
+                    mem.write_mask(mask_value, window=w)
+
+                    overviews = [2 ** j for j in range(1, overview_level + 1)]
+                    mem.build_overviews(overviews, rwarp.Resampling.nearest)
+                    mem.update_tags(ns='rio_oveview', resampling=rwarp.Resampling.nearest.value)
+
+            while True:
+                chunk = memfile.read(8192)
+                if not chunk:
+                    break
+
+                f.write(chunk)
 
     def _proxy_tms(self,tms):
         if 'TRAVIS' in os.environ:
