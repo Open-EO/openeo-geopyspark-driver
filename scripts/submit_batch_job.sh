ee9cad751421cac924a45d310065abf10a9d3074
Jan Van den bosch <jan@bossie.org>
2019-04-04 17:13:22 +0200

    build and submit batch job capable OpenEO

--- scripts/submit_batch_job.sh
+++ scripts/submit_batch_job.sh
@@ -23,18 +23,20 @@ outputFile=$3
 principal=$4
 keyTab=$5
 
+pysparkPython="venv/bin/python"
+
 kinit -kt ${keyTab} ${principal}
 
 export HDP_VERSION=3.0.0.0-1634
-export SPARK_HOME=/usr/hdp/$HDP_VERSION/spark2/
-export PYSPARK_PYTHON="./python"
+export SPARK_HOME=/usr/hdp/$HDP_VERSION/spark2
 export PATH="$SPARK_HOME/bin:$PATH"
 export SPARK_SUBMIT_OPTS="-Dlog4j.configuration=file:${LOG4J_CONFIGURATION_FILE}"
+export LD_LIBRARY_PATH="/opt/rh/rh-python35/root/usr/lib64"
 
 extensions=$(ls geotrellis-extensions-*.jar)
 backend_assembly=$(ls geotrellis-backend-assembly-*.jar)
 
-${SPARK_HOME}/bin/spark-submit \
+spark-submit \
  --master yarn --deploy-mode cluster \
  --principal ${principal} --keytab ${keyTab} \
  --conf spark.executor.memory=8G \
@@ -42,10 +44,12 @@ ${SPARK_HOME}/bin/spark-submit \
  --conf spark.speculation.quantile=0.4 --conf spark.speculation.multiplier=1.1 \
  --conf spark.dynamicAllocation.minExecutors=20 \
  --conf "spark.yarn.appMasterEnv.SPARK_HOME=$SPARK_HOME" --conf spark.yarn.appMasterEnv.PYTHON_EGG_CACHE=./ \
- --conf "spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYSPARK_PYTHON" \
+ --conf "spark.yarn.appMasterEnv.PYSPARK_PYTHON=$pysparkPython" \
+ --conf spark.executorEnv.LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64 \
+ --conf spark.yarn.appMasterEnv.LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64 \
  --conf spark.executorEnv.DRIVER_IMPLEMENTATION_PACKAGE=openeogeotrellis --conf spark.yarn.appMasterEnv.DRIVER_IMPLEMENTATION_PACKAGE=openeogeotrellis \
  --conf spark.locality.wait=300ms --conf spark.shuffle.service.enabled=true --conf spark.dynamicAllocation.enabled=true \
- --files python,$(ls typing-*-none-any.whl),layercatalog.json,"${processGraphFile}" \
+ --files $(ls typing-*-none-any.whl),layercatalog.json,"${processGraphFile}" \
  --archives "${OPENEO_VENV_ZIP}#venv" \
  --conf spark.hadoop.security.authentication=kerberos --conf spark.yarn.maxAppAttempts=1 \
  --jars "${extensions}","${backend_assembly}" \
