c35dd28ceb8d4d1c4e2ae75c6e4b1112cddfb48d
Jeroen Dries <jeroen.dries@vito.be>
2019-07-03 22:36:49 +0200

    replace wget with curl (wget not available on images)

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 mkdir -p jars
-export jar="geotrellis-backend-assembly-0.4.2.jar"
 cd jars
-wget -nc https://artifactory.vgt.vito.be/libs-release-public/org/openeo/geotrellis-extensions/1.1.0/geotrellis-extensions-1.1.0.jar
-wget -nc https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/$jar
+curl -O -C - https://artifactory.vgt.vito.be/libs-release-public/org/openeo/geotrellis-extensions/1.1.0/geotrellis-extensions-1.1.0.jar
+curl -O -C - https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/geotrellis-backend-assembly-0.4.2.jar
 cd ..
