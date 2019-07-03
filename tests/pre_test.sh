49f69e401bc5c8d3bb9aad41042f314664183c57
Jeroen Dries <jeroen.dries@vito.be>
2019-07-03 21:19:22 +0200

    fix download script

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mkdir -p jars
-mvn dependency:copy -Dartifact=org.openeo:geotrellis-extensions:1.1.0-SNAPSHOT -DoutputDirectory=jars
-jar = "geotrellis-backend-assembly-0.4.2.jar"
-curl -L https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/$jar -o jars/$jar
+export jar="geotrellis-backend-assembly-0.4.2.jar"
+cd jars
+wget -nc https://artifactory.vgt.vito.be/libs-release-public/org/openeo/geotrellis-extensions/1.1.0/geotrellis-extensions-1.1.0.jar
+wget -nc https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/$jar
+cd ..
