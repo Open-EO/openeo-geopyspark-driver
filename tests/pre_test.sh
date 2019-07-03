3bc3cd4058e934b75b15f31c415ca493cca0e7a3
Jeroen Dries <jeroen.dries@vito.be>
2019-07-03 20:49:20 +0200

    make pre_test script independent of geopyspark

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-geopyspark install-jar
+mkdir -p jars
+mvn dependency:copy -Dartifact=org.openeo:geotrellis-extensions:1.1.0-SNAPSHOT -DoutputDirectory=jars
+jar = "geotrellis-backend-assembly-0.4.2.jar"
+curl -L https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/$jar -o jars/$jar
