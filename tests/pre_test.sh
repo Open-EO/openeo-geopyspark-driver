a8347d5175aa8a01c8a513350e547fc1efd235fd
Jeroen Dries <jeroen.dries@vito.be>
2019-07-04 12:25:56 +0200

    follow redirects with curl to actually download the file

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p jars
 cd jars
-curl -O -C - https://artifactory.vgt.vito.be/libs-release-public/org/openeo/geotrellis-extensions/1.1.0/geotrellis-extensions-1.1.0.jar
-curl -O -C - https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/geotrellis-backend-assembly-0.4.2.jar
+curl -L -O -C - https://artifactory.vgt.vito.be/libs-release-public/org/openeo/geotrellis-extensions/1.1.0/geotrellis-extensions-1.1.0.jar
+curl -L -O -C - https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/geotrellis-backend-assembly-0.4.2.jar
 cd ..
