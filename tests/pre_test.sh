e6e1376b2350d8463d0f0886a82cbba2e5db3a61
Jeroen Dries <jeroen.dries@vito.be>
2019-07-05 14:54:19 +0200

    use updated version of backend assembly

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -3,5 +3,5 @@ runuser jenkins -c '
 mkdir -p jars
 cd jars
 curl -L -O -C - https://artifactory.vgt.vito.be/libs-release-public/org/openeo/geotrellis-extensions/1.1.0/geotrellis-extensions-1.1.0.jar
-curl -L -O -C - https://github.com/locationtech-labs/geopyspark/releases/download/v0.4.2/geotrellis-backend-assembly-0.4.2.jar
+curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.2.jar
 cd ..'
