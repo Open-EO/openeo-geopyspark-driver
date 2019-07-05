6c4130c9ad8ec998d4df07fc6230b5108e2c10e1
Jan Van den bosch <jan.vandenbosch@hotmail.com>
2019-07-05 13:29:16 +0200

    EP-2704: fixed "Band count must be greater than 0"

--- openeogeotrellis/__init__.py
+++ openeogeotrellis/__init__.py
@@ -108,7 +108,7 @@ def getImageCollection(product_id:str, viewingParameters):
     top = viewingParameters.get("top",None)
     bottom = viewingParameters.get("bottom",None)
     srs = viewingParameters.get("srs",None)
-    band_indices = viewingParameters.get("bands", [])
+    band_indices = viewingParameters.get("bands")
     pysc = gps.get_spark_context()
     extent = None
 
@@ -145,7 +145,9 @@ def getImageCollection(product_id:str, viewingParameters):
     temporal_tiled_raster_layer = jvm.geopyspark.geotrellis.TemporalTiledRasterLayer
     option = jvm.scala.Option
     levels = {pyramid.apply(index)._1():TiledRasterLayer(LayerType.SPACETIME,temporal_tiled_raster_layer(option.apply(pyramid.apply(index)._1()),pyramid.apply(index)._2())) for index in range(0,pyramid.size())}
-    return GeotrellisTimeSeriesImageCollection(gps.Pyramid(levels), _service_registry, catalog.catalog[product_id]).band_filter(band_indices)
+
+    image_collection = GeotrellisTimeSeriesImageCollection(gps.Pyramid(levels), _service_registry, catalog.catalog[product_id])
+    return image_collection.band_filter(band_indices) if band_indices else image_collection
 
 def create_process_visitor():
     from .geotrellis_tile_processgraph_visitor import GeotrellisTileProcessGraphVisitor
