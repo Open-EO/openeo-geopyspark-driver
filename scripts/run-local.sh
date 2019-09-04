b62b2671f86f74f1ef2fa54e495cad509c6b7dbd
Tamas Banyai <tamas.banyai@vito.be>
2019-09-04 10:15:33 +0200

    Make run-local.sh work again.

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=../openeo-python-driver/:../openeo-client-api:./:../geopyspark SPARK_HOME=$(find_spark_home.py)
-python openeogeotrellis/deploy/local.py
+# run it from the root of the openeo-gepyspark-driver
+
+
+export SPARK_HOME=$(find_spark_home.py)
+#export HADOOP_CONF_DIR=/etc/hadoop/conf
+export FLASK_DEBUG=1
+export DRIVER_IMPLEMENTATION_PACKAGE=openeogeotrellis
+export ZOOKEEPERNODES=localhost:2181
+
+#export PYTHONPATH=../openeo-python-driver/:../openeo-client-api:./:../geopyspark SPARK_HOME=$(find_spark_home.py)
+
+export PYTHONPATH=../openeo-udf/src/:../openeo-python-client/:../openeo-python-driver/:../openeo-client-api:./
+
+python3 openeogeotrellis/deploy/local.py
