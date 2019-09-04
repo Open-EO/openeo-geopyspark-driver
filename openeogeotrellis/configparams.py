5053fd80c50d8f22b60771ddbca3fdf8ef4d95c6
Tamas Banyai <tamas.banyai@vito.be>
2019-09-04 16:18:19 +0200

    Removing obsolete option in configparam.

--- openeogeotrellis/configparams.py
+++ openeogeotrellis/configparams.py
@@ -6,7 +6,6 @@ class ConfigParams:
         self.zookeepernodes=os.environ.get("ZOOKEEPERNODES",
             'epod-master1.vgt.vito.be:2181,epod-master2.vgt.vito.be:2181,epod-master3.vgt.vito.be:2181'    
         ).split(',') 
-        self.register_layers_to_accumulo=os.environ.get("REGISTER_LAYERS_TO_ACCUMULO","True").lower() in ('true','yes','1')
     
     
     
