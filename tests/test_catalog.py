7b50c0e58bb4b41bdd288c6a0266832be7e56b2a
Stefaan Lippens <stefaan.lippens@vito.be>
2019-09-05 14:47:02 +0200

    Fixes #19: add collection band metadata according to spec (`eo:bands` and `cube:dimensions`)

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -24,3 +24,19 @@ def test_layercatalog_json():
         # assert 'description' in layer
         # assert 'license' in layer
         # assert 'extent' in layer
+
+
+def test_issue77_band_metadata():
+    for layer in LayerCatalog().catalog.values():
+        # print(layer['id'])
+        # TODO: stop doing this non-standard band metadata ("bands" item in metadata root)
+        old_bands = [b if isinstance(b, str) else b["band_id"] for b in layer.get("bands", [])]
+        eo_bands = [b["name"] for b in layer.get("properties", {}).get('eo:bands', [])]
+        cube_dimension_bands = []
+        for cube_dim in layer.get("properties", {}).get("cube:dimensions", {}).values():
+            if cube_dim["type"] == "bands":
+                cube_dimension_bands = cube_dim["values"]
+        if len(old_bands) > 1:
+            assert old_bands == eo_bands
+            assert old_bands == cube_dimension_bands
+        assert eo_bands == cube_dimension_bands
