ba845820a068a26f28207aee139687729786f60d
Tamas Banyai <tamas.banyai@vito.be>
2019-09-09 08:27:04 +0200

    Out of source build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,40 +1,64 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+#set -exo pipefail
 
-export LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64:${LD_LIBRARY_PATH}
 unset PYTHONPATH
 
-python3.7 -m venv ../install/venv
-source ../install/venv/bin/activate
+INSTALL_DEPS="0"
 
-pip install --upgrade --force-reinstall pip
-pip download typing==3.6.6
-pip download Fiona==1.7.13 && pip install Fiona-1.7.13-cp37-cp37m-manylinux1_x86_64.whl
+INSTALLDIR=$(cd ../install ; pwd)
+echo $INSTALLDIR
+
+python3.7 -m venv $INSTALLDIR/venv
+. $INSTALLDIR/venv/bin/activate
+
+if [ "$INSTALL_DEPS" == "1" ] ; then
+  pip install --upgrade --force-reinstall pip
+  pip download typing==3.6.6
+  pip download Fiona==1.7.13 && pip install Fiona-1.7.13-cp37-cp37m-manylinux1_x86_64.whl
+fi
 
 cd ../openeo-python-client
 rm -rf dist/*
-pip install travis-sphinx==2.1.0 "sphinx<1.7"
-pip install -r requirements-dev.txt
-pip install -r requirements.txt
-python setup.py install bdist_egg
-cp dist/* ../install/
-
+if [ "$INSTALL_DEPS" == "1" ] ; then
+  pip install travis-sphinx==2.1.0 "sphinx<1.7"
+  pip install -r requirements-dev.txt
+  pip install -r requirements.txt
+fi 
+python3 setup.py install bdist_egg
+cp dist/* $INSTALLDIR/
 
 cd ../openeo-python-driver
 rm -rf dist/*
-pip install -r requirements-dev.txt
-pip install -r requirements.txt
-python setup.py install bdist_egg
-cp dist/* ../install/
+if [ "$INSTALL_DEPS" == "1" ] ; then
+  pip install -r requirements-dev.txt
+  pip install -r requirements.txt
+fi
+python3 setup.py install bdist_egg
+cp dist/* $INSTALLDIR/
 
 cd ../openeo-geopyspark-driver
 rm -rf dist/*
-rm -rf jars/*
-pip install $(cat requirements.txt | tr '\n' ' ' | sed -e 's/openeo-api==0.0.1/openeo-api/') --extra-index-url https://artifactory.vgt.vito.be/api/pypi/python-openeo/simple
-SPARK_HOME=$(find_spark_home.py) geopyspark install-jar
-#mkdir -p jars && mvn dependency:copy -Dartifact=org.openeo:geotrellis-extensions:1.1.0-SNAPSHOT -DoutputDirectory=jars
-mkdir -p jars && mvn dependency:get -DremoteRepositories=https://artifactory.vgt.vito.be/libs-snapshot-public/  -Dartifact=org.openeo:geotrellis-extensions:1.1.0-SNAPSHOT -Ddest=./jars
-python setup.py install bdist_egg
-cp dist/* ../install/
-cp jars/* ../install/
+#mkdir -p jars 
+#rm -rf jars/*
+if [ "$INSTALL_DEPS" == "1" ] ; then
+  pip install $(cat requirements.txt | tr '\n' ' ' | sed -e 's/openeo-api==0.0.1/openeo-api/') --extra-index-url https://artifactory.vgt.vito.be/api/pypi/python-openeo/simple
+  SPARK_HOME=$(find_spark_home.py) geopyspark install-jar
+  #mkdir -p jars && mvn dependency:copy -Dartifact=org.openeo:geotrellis-extensions:1.1.0-SNAPSHOT -DoutputDirectory=jars
+  #mvn dependency:get -DremoteRepositories=https://artifactory.vgt.vito.be/libs-snapshot-public/  -Dartifact=org.openeo:geotrellis-extensions:1.1.0-SNAPSHOT -Ddest=./jars
+  #mvn dependency:get -DremoteRepositories=https://artifactory.vgt.vito.be/auxdata-public/  -Dartifact=org.openeo:ggeotrellis-backend-assembly-0.4.2 -Ddest=./jars
+  curl https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/1.2.0-SNAPSHOT/geotrellis-extensions-1.2.0-SNAPSHOT.jar -O
+  curl https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.2.jar -O
+fi
+python3 setup.py install bdist_egg
+cp dist/* $INSTALLDIR/
+cp ./*.jar $INSTALLDIR/
+cp layercatalog.json $INSTALLDIR/
+mkdir -p $INSTALLDIR/scripts
+cp scripts/*.sh $INSTALLDIR/scripts/
+cp scripts/*.properties $INSTALLDIR/scripts/
+mkdir -p $INSTALLDIR/openeogeotrellis/deploy
+cp openeogeotrellis/deploy/*.py $INSTALLDIR/openeogeotrellis/deploy/
+
+echo "INSTALL SUCCESSFUL"
+
