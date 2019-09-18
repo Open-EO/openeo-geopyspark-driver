aef9741f270e69054042c928399fe11c7f468a97
Jeroen Dries <jeroen.dries@vito.be>
2019-09-18 09:00:28 +0200

    EP-2968 updated submit script for python 3.6

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -8,11 +8,11 @@ pysparkPython="venv/bin/python"
 export HDP_VERSION=3.1.0.0-78
 export SPARK_MAJOR_VERSION=2
 export SPARK_HOME=/usr/hdp/${HDP_VERSION}/spark2
-export LD_LIBRARY_PATH="/opt/rh/rh-python35/root/usr/lib64"
-export PYTHONPATH=""
+export LD_LIBRARY_PATH="venv/lib64"
 
+export PYTHONPATH="venv/lib64/python3.6/site-packages:venv/lib/python3.6/site-packages"
 
-hdfsVenvZip=https://artifactory.vgt.vito.be/auxdata-public/openeo/venv.zip
+hdfsVenvZip=https://artifactory.vgt.vito.be/auxdata-public/openeo/venv36.zip
 extensions=https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/1.2.0-SNAPSHOT/geotrellis-extensions-1.2.0-SNAPSHOT.jar
 backend_assembly=https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.2.jar
 
@@ -37,8 +37,8 @@ spark-submit \
  --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=${pysparkPython} \
  --conf spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON=${pysparkPython} \
  --conf spark.executorEnv.PYSPARK_PYTHON=${pysparkPython} \
- --conf spark.executorEnv.LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64 --conf spark.executorEnv.XDG_DATA_DIRS=/opt/rh/rh-python35/root/usr/share \
- --conf spark.yarn.appMasterEnv.LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64 --conf spark.yarn.appMasterEnv.XDG_DATA_DIRS=/opt/rh/rh-python35/root/usr/share \
+ --conf spark.executorEnv.LD_LIBRARY_PATH=venv/lib64  \
+ --conf spark.yarn.appMasterEnv.LD_LIBRARY_PATH=venv/lib64  \
  --conf spark.yarn.appMasterEnv.OPENEO_VENV_ZIP=${hdfsVenvZip} \
  --conf spark.executorEnv.DRIVER_IMPLEMENTATION_PACKAGE=openeogeotrellis --conf spark.yarn.appMasterEnv.DRIVER_IMPLEMENTATION_PACKAGE=openeogeotrellis \
  --conf spark.yarn.appMasterEnv.WMTS_BASE_URL_PATTERN=http://openeo.vgt.vito.be/openeo/services/%s \
