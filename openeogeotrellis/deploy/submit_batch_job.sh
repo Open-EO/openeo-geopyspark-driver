560db24f367c0fc56d76f170018a53b066f61d40
Jeroen Dries <jeroen.dries@vito.be>
2019-11-06 11:22:13 +0100

    EP-3103 driver needs even more memory to be able to stitch large files

--- openeogeotrellis/deploy/submit_batch_job.sh
+++ openeogeotrellis/deploy/submit_batch_job.sh
@@ -52,7 +52,7 @@ spark-submit \
  --master yarn --deploy-mode cluster \
  --principal ${principal} --keytab ${keyTab} \
  --conf spark.yarn.submit.waitAppCompletion=false \
- --driver-memory 14G \
+ --driver-memory 16G \
  --executor-memory 4G \
  --conf spark.driver.maxResultSize=5g \
  --conf spark.driver.memoryOverhead=1g \
