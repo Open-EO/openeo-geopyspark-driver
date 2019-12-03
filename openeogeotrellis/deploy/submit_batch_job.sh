9e2652714c9ae17300f9c7218aa5349ea5ac7d7c
Jeroen Dries <jeroen.dries@vito.be>
2019-12-03 10:03:15 +0100

    fix batch submit script: can't use a dash in bash

--- openeogeotrellis/deploy/submit_batch_job.sh
+++ openeogeotrellis/deploy/submit_batch_job.sh
@@ -27,8 +27,8 @@ outputFile=$3
 principal=$4
 keyTab=$5
 apiVersion=$6
-driver-memory=${7-22G}
-executor-memory=${8-4G}
+drivermemory=${7-22G}
+executormemory=${8-4G}
 
 pysparkPython="venv/bin/python"
 
@@ -54,8 +54,8 @@ spark-submit \
  --master yarn --deploy-mode cluster \
  --principal ${principal} --keytab ${keyTab} \
  --conf spark.yarn.submit.waitAppCompletion=false \
- --driver-memory ${driver-memory} \
- --executor-memory ${executor-memory} \
+ --driver-memory ${drivermemory} \
+ --executor-memory ${executormemory} \
  --driver-java-options "-Dscala.concurrent.context.maxThreads=8" \
  --conf spark.driver.cores=4 \
  --conf spark.driver.maxResultSize=5g \
