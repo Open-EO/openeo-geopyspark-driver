1bfebd59c80549a9e4767faf19014dc6b1f3c2a7
Stefaan Lippens <stefaan.lippens@vito.be>
2020-07-06 10:42:18 +0200

    fixup! Use rfc3339 datetime utility from openeo package

--- tests/test_views.py
+++ tests/test_views.py
@@ -93,7 +93,7 @@ class TestCollections:
             assert resp['stac_version'] == "0.6.2"
             assert resp['extent'] == {
                 "spatial": [-180, -56, 180, 83],
-                "temporal": ["2015-07-06", None]
+                "temporal": ["2015-07-06T00:00:00Z", None]
             }
             assert resp["properties"]['cube:dimensions'] == cube_dims
             for f in eo_bands[0].keys():
