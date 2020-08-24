1737fcf103d7bcf4923aa866775761b264befc52
Jeroen Dries <jeroen.dries@vito.be>
2020-08-24 10:38:41 +0200

    EP-3551 test for merge with spatial cube

--- tests/test_apply_process.py
+++ tests/test_apply_process.py
@@ -417,6 +417,30 @@ class TestCustomFunctions(TestCase):
         expected[0][0]=255.0
         np.testing.assert_array_almost_equal(cells, expected.astype(np.uint8))
 
+    def test_merge_cubes_spatial(self):
+        red_ramp, nir_ramp = np.mgrid[0:4, 0:4]
+        layer1 = self._create_spacetime_layer(cells=np.array([[red_ramp]]))
+        layer2 = self._create_spacetime_layer(cells=np.array([[nir_ramp]])).to_spatial_layer()
+
+        metadata = CollectionMetadata({
+            "cube:dimensions": {
+                # TODO: also specify other dimensions?
+                "bands": {"type": "bands", "values": ["the_band"]}
+            },
+            "summaries": {
+                "eo:bands": [
+                    {"name": "the_band"}
+                ]
+            }
+        })
+
+        cube1 = GeotrellisTimeSeriesImageCollection(gps.Pyramid({0: layer1}), InMemoryServiceRegistry(), metadata=metadata)
+        cube2 = GeotrellisTimeSeriesImageCollection(gps.Pyramid({0: layer2}), InMemoryServiceRegistry(), metadata=metadata)
+        sum = cube1.merge(cube2,'subtract')
+        stitched = sum.pyramid.levels[0].to_spatial_layer().stitch()
+
+        np.testing.assert_array_equal(red_ramp - nir_ramp, stitched.cells[0, 0:4, 0:4])
+
     def test_merge_cubes_into_single_band(self):
         red_ramp, nir_ramp = np.mgrid[0:4, 0:4]
         layer1 = self._create_spacetime_layer(cells=np.array([[red_ramp]]))
