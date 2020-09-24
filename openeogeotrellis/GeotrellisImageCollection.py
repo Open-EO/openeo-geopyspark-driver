681796266a4f3ad38c070347ae929bc68e6fd1d3
Jeroen Dries <jeroen.dries@vito.be>
2020-09-24 13:28:59 +0200

    EP-3548 make sure resulting geotiff extent matches with user request

--- openeogeotrellis/GeotrellisImageCollection.py
+++ openeogeotrellis/GeotrellisImageCollection.py
@@ -957,7 +957,9 @@ class GeotrellisTimeSeriesImageCollection(ImageCollection):
                 band_count = 1
                 if self.metadata.has_band_dimension():
                     band_count = len(self.metadata.band_dimension.band_names)
-                self._get_jvm().org.openeo.geotrellis.geotiff.package.saveRDD(spatial_rdd.srdd.rdd(),band_count,filename,zlevel)
+                crop_extent = self._get_jvm().geotrellis.vector.Extent(crop_bounds.xmin,crop_bounds.ymin,crop_bounds.xmax,crop_bounds.ymax)
+                cropped_cube = self._get_jvm().org.openeo.geotrellis.OpenEOProcesses().crop_spatial(spatial_rdd.srdd.rdd(),crop_extent)
+                self._get_jvm().org.openeo.geotrellis.geotiff.package.saveRDD(cropped_cube,band_count,filename,zlevel,self._get_jvm().scala.Option.apply(crop_extent))
             else:
                 self._save_stitched(spatial_rdd, filename, crop_bounds,zlevel=zlevel)
 
