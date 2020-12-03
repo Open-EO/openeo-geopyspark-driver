bc1527c9cf28695111be62464f32282d94923b95
tcassaert <tcassaert@inuits.eu>
2020-12-03 15:12:15 +0100

    Added batch job registering and tracking

--- openeogeotrellis/deploy/kube.py
+++ openeogeotrellis/deploy/kube.py
@@ -31,6 +31,8 @@ dictConfig({
 
 
 from openeo_driver import server
+from openeogeotrellis.job_tracker import JobTracker
+from openeogeotrellis.job_registry import JobRegistry
 
 log = logging.getLogger("openeo-geopyspark-driver.kubernetes")
 
@@ -45,10 +47,23 @@ def main():
     host, _ = deploy.get_socket()
     port = 50001 if not 'KUBE_OPENEO_API_PORT' in os.environ else os.environ['KUBE_OPENEO_API_PORT']
 
+    def setup_batch_jobs() -> None:
+#        principal = sc.getConf().get("spark.yarn.principal")
+#        keytab = sc.getConf().get("spark.yarn.keytab")
+
+        with JobRegistry() as job_registry:
+            job_registry.ensure_paths()
+
+        job_tracker = JobTracker(JobRegistry, "", "")
+        threading.Thread(target=job_tracker.update_statuses, daemon=True).start()
+
     def on_started() -> None:
         from openeo_driver.views import app
 
         app.logger.setLevel('DEBUG')
+        deploy.load_custom_processes(app.logger)
+
+        setup_batch_jobs()
 
     server.run(title="OpenEO API",
                description="""[UNSTABLE] OpenEO API running on CreoDIAS (using GeoPySpark driver). This endpoint runs openEO on a Kubernetes cluster.
