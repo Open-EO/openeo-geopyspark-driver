24ca4cacbc5806cc177b24068137b0bf22d4b874
Jeroen Dries <jeroen.dries@vito.be>
2020-12-03 10:14:33 +0100

    use jars built against scala 2.12

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -2,6 +2,6 @@
 runuser jenkins -c '
 mkdir -p jars
 cd jars
-curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.0.0-SNAPSHOT/geotrellis-extensions-2.0.0-SNAPSHOT.jar
-curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo.jar
+curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-extensions-2.0.0_2.12-SNAPSHOT.jar
+curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
 cd ..'
