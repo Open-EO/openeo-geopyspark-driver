e43a0d07f920778f5467c1fb040c2a8051b1999e
Jeroen Dries <jeroen.dries@vito.be>
2021-01-04 11:53:57 +0100

    download correct jar depending on environment

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-yum info hdf5
 yum install -y hdf5
 runuser jenkins -c '
 mkdir -p jars
 cd jars
-curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-extensions-2.0.0_2.12-SNAPSHOT.jar
-curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
+if [ $(lsb_release -rs) = "8.2.2004" ]; then
+  curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.0.0_2.12-SNAPSHOT/geotrellis-extensions-2.0.0_2.12-SNAPSHOT.jar
+  curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
+else
+ curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.0.0-SNAPSHOT/geotrellis-extensions-2.0.0-SNAPSHOT.jar
+ curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo.jar
+fi
 cd ..'
