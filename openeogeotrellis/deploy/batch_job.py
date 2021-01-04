926e73e3c09839c15b8f5d46e6fd04ef1035f619
Jeroen Dries <jeroen.dries@vito.be>
2021-01-04 11:08:30 +0100

    try to fix class reference

--- openeogeotrellis/deploy/batch_job.py
+++ openeogeotrellis/deploy/batch_job.py
@@ -186,7 +186,7 @@ def main(argv: List[str]) -> None:
         conf = (SparkConf()
                 .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
                 .set(key='spark.kryo.registrator', value='geopyspark.geotools.kryo.ExpandedKryoRegistrator')
-                .set("spark.kryo.classesToRegister", "org.openeo.geotrellisaccumulo.SerializableConfiguration,ar.com.hjg.pngj.ImageInfo,ar.com.hjg.pngj.ImageLineInt","geotrellis.raster.RasterRegion.GridBoundsRasterRegion"))
+                .set("spark.kryo.classesToRegister", "org.openeo.geotrellisaccumulo.SerializableConfiguration,ar.com.hjg.pngj.ImageInfo,ar.com.hjg.pngj.ImageLineInt","geotrellis.raster.RasterRegion$GridBoundsRasterRegion"))
 
         with SparkContext(conf=conf) as sc:
             kerberos()
