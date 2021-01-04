fb954b5f6e13561a838ae967e9bb2f384033db50
Jeroen Dries <jeroen.dries@vito.be>
2021-01-04 14:45:17 +0100

    constrain pyproj version, debugging projection issue

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'pandas>=0.24.2',
         'matplotlib==3.3.3',
         'geopandas~=0.7.0',
-        'pyproj>=2.2.0',
+        'pyproj>=2.2.0,<3.0.0',
         'protobuf~=3.6.0',
         'kazoo~=2.8.0',
         'rasterio~=1.1.8',
