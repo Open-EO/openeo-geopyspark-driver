3f65a0bc2faac27b9992135b32088e5661c6cc01
Jeroen Dries <jeroen.dries@vito.be>
2021-02-08 08:26:47 +0100

    also use next extensions version on centos 8

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -5,7 +5,7 @@ mkdir -p jars
 cd jars
 rm *.jar
 if [ $(lsb_release -rs) = "8.2.2004" ]; then
-  curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.1.0_2.12-SNAPSHOT/geotrellis-extensions-2.1.0_2.12-SNAPSHOT.jar
+  curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.2.0_2.12-SNAPSHOT/geotrellis-extensions-2.2.0_2.12-SNAPSHOT.jar
   curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
 else
  curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.2.0-SNAPSHOT/geotrellis-extensions-2.2.0-SNAPSHOT.jar
