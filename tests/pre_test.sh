5c82e322156c11b6eb2b85c406cdec11e70f39fb
Jan Van den bosch <jan@bossie.org>
2021-03-17 09:51:34 +0100

    don't run tests against unused Scala 2.12 jars
    py4j.Py4JException: Method pyramid_seq([class geotrellis.vector.Extent,
    class java.lang.String, class java.lang.String, class java.lang.String,
    class java.util.ArrayList, class java.util.HashMap]) does not exist

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -4,11 +4,6 @@ runuser jenkins -c '
 mkdir -p jars
 cd jars
 rm *.jar
-if [ $(lsb_release -rs) = "8.2.2004" ]; then
-  curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.2.0_2.12-SNAPSHOT/geotrellis-extensions-2.2.0_2.12-SNAPSHOT.jar
-  curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
-else
- curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.2.0-SNAPSHOT/geotrellis-extensions-2.2.0-SNAPSHOT.jar
- curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo.jar
-fi
+curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.2.0-SNAPSHOT/geotrellis-extensions-2.2.0-SNAPSHOT.jar
+curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo.jar
 cd ..'
