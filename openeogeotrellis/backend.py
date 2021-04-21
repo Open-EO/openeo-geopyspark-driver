234caa7e0003e09abf81cc3db9bee318bf6cbac2
Stefaan Lippens <stefaan.lippens@vito.be>
2021-04-21 14:12:28 +0200

    OIDC: start requesting the "email" scope too

--- openeogeotrellis/backend.py
+++ openeogeotrellis/backend.py
@@ -268,7 +268,7 @@ class GeoPySparkBackendImplementation(backend.OpenEoBackendImplementation):
             OidcProvider(
                 id="egi",
                 issuer="https://aai.egi.eu/oidc/",
-                scopes=["openid"],
+                scopes=["openid", "email"],
                 title="EGI Check-in",
                 default_client=default_client_egi,  # TODO: remove this legacy experimental field
                 default_clients=[default_client_egi],
@@ -277,7 +277,7 @@ class GeoPySparkBackendImplementation(backend.OpenEoBackendImplementation):
             OidcProvider(
                 id="egi-dev",
                 issuer="https://aai-dev.egi.eu/oidc/",
-                scopes=["openid"],
+                scopes=["openid", "email"],
                 title="EGI Check-in (dev)",
                 default_client=default_client_egi,   # TODO: remove this legacy experimental field
                 default_clients=[default_client_egi],
@@ -285,7 +285,7 @@ class GeoPySparkBackendImplementation(backend.OpenEoBackendImplementation):
             OidcProvider(
                 id="keycloak",
                 issuer="https://sso.vgt.vito.be/auth/realms/terrascope",
-                scopes=["openid"],
+                scopes=["openid", "email"],
                 title="VITO Keycloak",
             ),
         ]
