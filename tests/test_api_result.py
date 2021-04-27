3af767d038a5588ff158ec9e817333e076630701
Stefaan Lippens <stefaan.lippens@vito.be>
2021-04-27 15:28:00 +0200

    EP-3781 add test about executing UDP with simple temporal reduce

--- tests/test_api_result.py
+++ tests/test_api_result.py
@@ -2,9 +2,21 @@ import numpy as np
 import pytest
 from numpy.testing import assert_equal
 
-from openeo_driver.testing import ApiTester
+from openeo_driver.backend import OpenEoBackendImplementation, UserDefinedProcesses
+from openeo_driver.testing import ApiTester, TEST_USER
 from openeo_driver.views import app
 from .data import TEST_DATA_ROOT
+
+
+@pytest.fixture
+def backend_implementation() -> OpenEoBackendImplementation:
+    import openeo_driver.views
+    return openeo_driver.views.backend_implementation
+
+
+@pytest.fixture
+def user_defined_process_registry(backend_implementation: OpenEoBackendImplementation) -> UserDefinedProcesses:
+    return backend_implementation.user_defined_processes
 
 
 @pytest.fixture
@@ -29,7 +41,7 @@ def test_execute_math_basic(api100):
     assert res.json == 8
 
 
-def test_execute_load_collection_json_basic(api100):
+def test_load_collection_json_basic(api100):
     res = api100.check_result({
         "lc": {
             "process_id": "load_collection",
@@ -56,3 +68,58 @@ def test_execute_load_collection_json_basic(api100):
         [[0, 0, 0, 0], [0.25, 0.25, 0.25, 0.25], [0.5, 0.5, 0.5, 0.5], [0.75, 0.75, 0.75, 0.75]],
         5 * np.ones((4, 4))
     ]])
+
+
+def test_udp_simple_temporal_reduce(api100, user_defined_process_registry):
+    """Test calling a UDP with simple temporal reduce operation"""
+    udp_spec = {
+        "id": "reduce_t_max",
+        "parameters": [
+            {"name": "data", "schema": {"type": "object", "subtype": "raster-cube"}}
+        ],
+        "process_graph": {
+            "reduce": {
+                "process_id": "reduce_dimension",
+                "arguments": {
+                    "data": {"from_parameter": "data"},
+                    "dimension": "t",
+                    "reducer": {
+                        "process_graph": {
+                            "max": {
+                                "process_id": "max", "arguments": {"data": {"from_parameter": "data"}}, "result": True
+                            }
+                        }
+                    }
+                },
+                "result": True
+            }
+        }
+    }
+    user_defined_process_registry.save(user_id=TEST_USER, process_id="reduce_t_max", spec=udp_spec)
+
+    res = api100.check_result({
+        "lc": {
+            "process_id": "load_collection",
+            "arguments": {
+                "id": "TestCollection-LonLat4x4",
+                "temporal_extent": ["2021-01-01", "2021-02-01"],
+                "spatial_extent": {"west": 0.0, "south": 0.0, "east": 1.0, "north": 1.0},
+                "bands": ["Longitude", "Day"]
+            },
+        },
+        "udp": {
+            "process_id": "reduce_t_max", "arguments": {"data": {"from_node": "lc"}}
+        },
+        "save": {
+            "process_id": "save_result",
+            "arguments": {"data": {"from_node": "udp"}, "format": "json"},
+            "result": True,
+        }
+    })
+    res.assert_status_code(200)
+    data = res.json
+    assert data["dims"] == ["bands", "x", "y"]
+    assert_equal(data["data"], np.array([
+        np.array([[0, .25, .5, .75]] * 4).T,
+        np.full((4, 4), fill_value=25)
+    ]))
