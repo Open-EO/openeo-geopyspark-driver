42a561d9e2dcbd9cb035588804135fc6082e858d
Jan Van den bosch <jan@bossie.org>
2021-06-17 11:25:47 +0200

    EP-3690: support polling SHub + spark-submit from the job_tracker process

--- scripts/job_tracker.sh
+++ scripts/job_tracker.sh
@@ -2,12 +2,16 @@
 
 set -eo pipefail
 
-source venv36/bin/activate
+source venv/bin/activate
 
-export SPARK_HOME=$(find_spark_home.py)
-export OPENEO_CATALOG_FILES="venv36/layercatalog.json"
+export SPARK_HOME="$(find_spark_home.py)"
+export OPENEO_CATALOG_FILES="layercatalog.json"
 export AWS_REGION="eu-central-1"
 export AWS_ACCESS_KEY_ID="???"
 export AWS_SECRET_ACCESS_KEY="!!!"
+export HADOOP_CONF_DIR="/etc/hadoop/conf"
+export OPENEO_VENV_ZIP="https://artifactory.vgt.vito.be/auxdata-public/openeo/dev/openeo-20210614-268.zip"
 
-python -m openeogeotrellis.job_tracker
+classpath="geotrellis-extensions-2.2.0-SNAPSHOT.jar:$(find $SPARK_HOME/jars -name '*.jar' | tr '\n' ':')"
+
+python -m openeogeotrellis.job_tracker --py4j-classpath "$classpath"
