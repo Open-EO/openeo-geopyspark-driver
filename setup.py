d4fdc62f3fcea7866d98b98a9c8bd01beb979e49
Stefaan Lippens <stefaan.lippens@vito.be>
2021-07-13 20:27:45 +0200

    EP-3856/#93 move `openeo_udf` to test dependencies

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ tests_require = [
     'schema',
     'scipy>=1.3.0',
     'requests-mock>=1.8.0',
+    'openeo_udf>=1.0.0rc3',
 ]
 
 setup(
@@ -39,7 +40,6 @@ setup(
     install_requires=[
         'openeo>=0.8.1a2.*',
         'openeo_driver>=0.10.11a1.*',
-        'openeo_udf>=1.0.0rc3',
         'pyspark==3.1.1; python_version>="3.8"',
         'pyspark>=2.3.1,<2.4.0; python_version<"3.8"',
         'geopyspark==0.4.7+openeo',
