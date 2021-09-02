dd0dc0f3bb5c67c2ff6fb34ae9ca6ac5c3853de3
Stefaan Lippens <stefaan.lippens@vito.be>
2021-09-02 11:38:13 +0200

    EP-3730 various naming/logging tweaks

--- openeogeotrellis/collections/s1backscatter_orfeo.py
+++ openeogeotrellis/collections/s1backscatter_orfeo.py
@@ -199,7 +199,7 @@ class S1BackscatterOrfeo:
     def _creo_scan_for_band_tiffs(creo_path: pathlib.Path, log_prefix: str) -> Dict[str, pathlib.Path]:
         """
         Scan given creodias path for TIFF files
-        :param creo_path: path to observation root folder
+        :param creo_path: path to product root folder
         :param log_prefix: prefix for logging
         :return: dictionary mapping band name (vv, vh, ...) to tiff path
         """
@@ -328,7 +328,10 @@ class S1BackscatterOrfeo:
                 data = ds.read(1)
                 nodata = ds.nodata
 
-        logger.info(f"{log_prefix} Final orfeo pipeline data: shape {data.shape}, min {numpy.nanmin(data)}, max {numpy.nanmax(data)}")
+        logger.info(
+            f"{log_prefix} Final orfeo pipeline result: shape {data.shape},"
+            f" min {numpy.nanmin(data)}, max {numpy.nanmax(data)}"
+        )
         return data, nodata
 
     def creodias(
@@ -346,6 +349,7 @@ class S1BackscatterOrfeo:
         """
         Implementation of S1 backscatter calculation with Orfeo in Creodias environment
         """
+        logger.info(f"{self.__class__.__name__}.creodias()")
         # Initial argument checking
         bands = bands or ["VH", "VV"]
         sar_calibration_lut = self._get_sar_calibration_lut(sar_backscatter_arguments.coefficient)
@@ -569,7 +573,7 @@ class S1BackscatterOrfeoV2(S1BackscatterOrfeo):
         if debug_mode:
             self._debug_show_rdd_info(feature_pyrdd)
 
-        # Group multiple tiles by observation
+        # Group multiple tiles by product id
         def process_feature(feature: dict) -> Tuple[str, dict]:
             creo_path = feature["feature"]["id"]
             return creo_path, {
@@ -578,19 +582,19 @@ class S1BackscatterOrfeoV2(S1BackscatterOrfeo):
                 "key_epsg": feature["metadata"]["crs_epsg"]
             }
 
-        per_obs = feature_pyrdd.map(process_feature).groupByKey().mapValues(list)
-        # TODO: still split if union is too large?
+        per_product = feature_pyrdd.map(process_feature).groupByKey().mapValues(list)
 
-        # Apply Orfeo processing over union of tiles per observation
-        # and slice up after that
+        # TODO: still split if full layout extent is too large for processing as a whole?
+
+        # Apply Orfeo processing over product files as whole and splice up in tiles after that
         @epsel.ensure_info_logging
-        @TimingLogger(title="process_obs", logger=logger)
-        def process_obs(obs: Tuple[str, List[dict]]):
-            creo_path, features = obs
+        @TimingLogger(title="process_product", logger=logger)
+        def process_product(product: Tuple[str, List[dict]]):
+            creo_path, features = product
 
-            # Short ad-hoc observation id.
-            obs_id = re.sub(r"[^A-Z0-9]", "", creo_path.upper())[-8:]
-            log_prefix = f"p{os.getpid()}-obs{obs_id}"
+            # Short ad-hoc product id for logging purposes.
+            prod_id = re.sub(r"[^A-Z0-9]", "", creo_path.upper())[-10:]
+            log_prefix = f"p{os.getpid()}-prod{prod_id}"
             logger.info(f"{log_prefix} creo path {creo_path}")
             logger.info(f"{log_prefix} sar_backscatter_arguments: {sar_backscatter_arguments!r}")
 
@@ -598,79 +602,88 @@ class S1BackscatterOrfeoV2(S1BackscatterOrfeo):
             if not creo_path.exists():
                 raise OpenEOApiException("Creo path does not exist")
 
-            # Union bbox around tiles
+            # Get whole extent of tile layout
             col_min = min(f["key"]["col"] for f in features)
             col_max = max(f["key"]["col"] for f in features)
+            cols = col_max - col_min + 1
             row_min = min(f["key"]["row"] for f in features)
             row_max = max(f["key"]["row"] for f in features)
+            rows = row_max - row_min + 1
             instants = set(f["key"]["instant"] for f in features)
             assert len(instants) == 1, f"Not single instant: {instants}"
             instant = instants.pop()
-            logger.info(f"{log_prefix} Union tiling keys col[{col_min},{col_max}] row[{row_min},{row_max}] instant[{instant}]")
+            logger.info(
+                f"{log_prefix} Layout key extent: col[{col_min}:{col_max}] row[{row_min}:{row_max}]"
+                f" ({cols}x{rows}={cols * rows} tiles) instant[{instant}]."
+            )
+
             xmin_min = min(f["key_extent"]["xmin"] for f in features)
             xmax_max = max(f["key_extent"]["xmax"] for f in features)
             ymin_min = min(f["key_extent"]["ymin"] for f in features)
             ymax_max = max(f["key_extent"]["ymax"] for f in features)
             key_epsgs = set(f["key_epsg"] for f in features)
             assert len(key_epsgs) == 1, f"Multiple key CRSs {key_epsgs}"
-            union_extent = {"xmin": xmin_min, "xmax": xmax_max, "ymin": ymin_min, "ymax": ymax_max}
-            union_epsg = key_epsgs.pop()
-            union_width = tile_size * (col_max - col_min + 1)
-            union_height = tile_size * (row_max - row_min + 1)
-            logger.info(f"{log_prefix} Union extent {union_extent} EPSG {union_epsg}: {union_width}x{union_height}px")
+            layout_extent = {"xmin": xmin_min, "xmax": xmax_max, "ymin": ymin_min, "ymax": ymax_max}
+            layout_epsg = key_epsgs.pop()
+            layout_width_px = tile_size * (col_max - col_min + 1)
+            layout_height_px = tile_size * (row_max - row_min + 1)
+            logger.info(
+                f"{log_prefix} Layout extent {layout_extent} EPSG {layout_epsg}:"
+                f" {layout_width_px}x{layout_height_px}px"
+            )
 
             band_tiffs = S1BackscatterOrfeo._creo_scan_for_band_tiffs(creo_path, log_prefix)
 
             dem_dir_context = S1BackscatterOrfeo._get_dem_dir_context(
                 sar_backscatter_arguments=sar_backscatter_arguments,
-                extent=union_extent,
-                epsg=union_epsg
+                extent=layout_extent,
+                epsg=layout_epsg
             )
 
             msg = f"{log_prefix} Process {creo_path} "
             with TimingLogger(title=msg, logger=logger), dem_dir_context as dem_dir:
                 # Allocate numpy array tile
-                orfeo_output_bands = numpy.zeros((len(bands), union_height, union_width), dtype=result_dtype)
+                orfeo_bands = numpy.zeros((len(bands), layout_height_px, layout_width_px), dtype=result_dtype)
 
                 for b, band in enumerate(bands):
                     if band.lower() not in band_tiffs:
                         raise OpenEOApiException(f"No tiff for band {band}")
                     data, nodata = S1BackscatterOrfeoV2._orfeo_pipeline(
                         input_tiff=band_tiffs[band.lower()],
-                        extent=union_extent, extent_epsg=union_epsg,
+                        extent=layout_extent, extent_epsg=layout_epsg,
                         dem_dir=dem_dir,
-                        extent_width_px=union_width, extent_height_px=union_height,
+                        extent_width_px=layout_width_px, extent_height_px=layout_height_px,
                         sar_calibration_lut=sar_calibration_lut,
                         noise_removal=noise_removal,
                         elev_geoid=elev_geoid, elev_default=elev_default,
                         log_prefix=f"{log_prefix}-{band}"
                     )
-                    orfeo_output_bands[b] = data
+                    orfeo_bands[b] = data
 
                 if sar_backscatter_arguments.options.get("to_db", False):
                     # TODO: keep this "to_db" shortcut feature or drop it
                     #       and require user to use standard openEO functionality (`apply` based conversion)?
                     logger.info(f"{log_prefix} Converting backscatter intensity to decibel")
-                    orfeo_output_bands = 10 * numpy.log10(orfeo_output_bands)
+                    orfeo_bands = 10 * numpy.log10(orfeo_bands)
 
                 # Split orfeo output in tiles
-                logger.info(f"{log_prefix} Split {orfeo_output_bands.shape} in tiles of {tile_size}")
-                cell_type = geopyspark.CellType(orfeo_output_bands.dtype.name)
+                logger.info(f"{log_prefix} Split {orfeo_bands.shape} in tiles of {tile_size}")
+                cell_type = geopyspark.CellType(orfeo_bands.dtype.name)
                 tiles = []
                 for c in range(col_max - col_min + 1):
                     for r in range(row_max - row_min + 1):
                         col = col_min + c
                         row = row_min + r
                         key = geopyspark.SpaceTimeKey(col=col, row=row, instant=_instant_ms_to_day(instant))
-                        tile = orfeo_output_bands[:, r * tile_size:(r + 1) * tile_size, c * tile_size:(c + 1) * tile_size]
+                        tile = orfeo_bands[:, r * tile_size:(r + 1) * tile_size, c * tile_size:(c + 1) * tile_size]
                         logger.info(f"{log_prefix} Create Tile for key {key} from {tile.shape}")
                         tile = geopyspark.Tile(tile, cell_type, no_data_value=nodata)
                         tiles.append((key, tile))
 
-            logger.info(f"{log_prefix} Union extent split in {len(tiles)} tiles")
+            logger.info(f"{log_prefix} Layout extent split in {len(tiles)} tiles")
             return tiles
 
-        tile_rdd = per_obs.flatMap(process_obs)
+        tile_rdd = per_product.flatMap(process_product)
         if result_dtype:
             layer_metadata_py.cell_type = result_dtype
         logger.info("Constructing TiledRasterLayer from numpy rdd, with metadata {m!r}".format(m=layer_metadata_py))
