f7610d77a6d3253667471f5ec369b28384552612
Jeroen Dries <jeroen.dries@vito.be>
2021-09-22 11:25:53 +0200

    fix metadata lookup, py4j vs scala issue https://github.com/Open-EO/openeo-geopyspark-driver/issues/58

--- openeogeotrellis/geopysparkdatacube.py
+++ openeogeotrellis/geopysparkdatacube.py
@@ -876,7 +876,7 @@ class GeopysparkDataCube(DriverDataCube):
             if target.pyramid.layer_type == gps.LayerType.SPACETIME:
                 partitioner = max_level.srdd.rdd().partitioner
             level_rdd_tuple = self._get_jvm().org.openeo.geotrellis.OpenEOProcesses().resampleCubeSpatial(
-                max_level.srdd.rdd(), target_max_level.srdd.rdd().metadata.crs, target_max_level.srdd.rdd().metadata.layout, resample_method, partitioner)
+                max_level.srdd.rdd(), target_max_level.srdd.rdd().metadata().crs, target_max_level.srdd.rdd().metadata().layout, resample_method, partitioner)
         else:
             raise FeatureUnsupportedException(message='resample_cube_spatial - Unsupported combination of two cubes of type: ' + self.pyramid.layer_type + ' and ' + target.pyramid.layer_type)
 
