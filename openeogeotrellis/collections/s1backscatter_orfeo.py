c0712326022237b88cc74660e8ce7a6fbc72831c
Jeroen Dries <jeroen.dries@vito.be>
2021-10-04 12:23:29 +0200

    revert previous change, trying to find cause of artifacts

--- openeogeotrellis/collections/s1backscatter_orfeo.py
+++ openeogeotrellis/collections/s1backscatter_orfeo.py
@@ -288,7 +288,7 @@ class S1BackscatterOrfeo:
                 sar_calibration.SetParameterString("in", str(input_tiff))
                 sar_calibration.SetParameterString("lut", sar_calibration_lut)
                 #sar_calibration.SetParameterValue('removenoise', noise_removal)
-                sar_calibration.SetParameterInt('ram', 256)
+                sar_calibration.SetParameterInt('ram', 512)
                 logger.info(f"{log_prefix} SARCalibration params: {otb_param_dump(sar_calibration)}")
                 sar_calibration.Execute()
 
@@ -312,7 +312,7 @@ class S1BackscatterOrfeo:
                 ortho_rect.SetParameterInt("outputs.uly", int(extent["ymax"]))
                 ortho_rect.SetParameterString("interpolator", "linear")
                 ortho_rect.SetParameterFloat("opt.gridspacing", 40.0)
-                ortho_rect.SetParameterInt("opt.ram", 256)
+                ortho_rect.SetParameterInt("opt.ram", 512)
                 logger.info(f"{log_prefix} OrthoRectification params: {otb_param_dump(ortho_rect)}")
                 ortho_rect.Execute()
 
