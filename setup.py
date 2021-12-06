b41021769b029bda023ab43d799778497f6394e9
Jeroen Dries <jeroen.dries@vito.be>
2021-12-06 09:51:59 +0100

    constrain pandas version, to avoid unexpected upgrades

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'geopyspark==0.4.7+openeo',
         'py4j',
         'numpy>=1.17.0,<1.19.0',
-        'pandas>=0.24.2',
+        'pandas>=0.24.2,<1.4.0',
         'matplotlib==3.3.3',
         'geopandas~=0.7.0',
         'pyproj>=2.2.0,<3.0.0',
