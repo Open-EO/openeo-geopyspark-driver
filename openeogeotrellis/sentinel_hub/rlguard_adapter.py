8f988b2ac96daa961ec7c8b911a168cf619994f1
Jan Van den bosch <jan@bossie.org>
2021-12-08 13:14:01 +0100

    support logging of processing units

--- openeogeotrellis/sentinel_hub/rlguard_adapter.py
+++ openeogeotrellis/sentinel_hub/rlguard_adapter.py
@@ -9,25 +9,33 @@ from rlguard import apply_for_request, calculate_processing_units, OutputFormat,
 from rlguard.repository import ZooKeeperRepository
 
 
-def calculate_delay(request_params):
+def request(request_params) -> (float, float):
     request_params['output_format'] = OutputFormat(request_params['output_format'])
+
+    pu = calculate_processing_units(**request_params)
 
     with zk_client() as zk:
         zookeeper_repository = ZooKeeperRepository(zk, key_base="/openeo/rlguard")
+        delay = apply_for_request(pu, zookeeper_repository)
 
-        pu = calculate_processing_units(**request_params)
-        return apply_for_request(pu, zookeeper_repository)
+        return pu, delay
 
 
-if __name__ == '__main__':
+def main():
     if len(argv) < 2:
         print(f"Usage: {argv[0]} <request params JSON>", file=sys.stderr)
         exit(1)
 
+    request_params = json.loads(argv[1])
+
     try:
-        delay = calculate_delay(json.loads(argv[1]))
-        print(json.dumps({'delay_s': delay}))
-    except SyncerDownException as e:
+        pu, delay = request(request_params)
+        print(json.dumps({'processing_units': pu, 'delay_s': delay}))
+    except SyncerDownException:
         print(json.dumps({'error': traceback.format_exc()}))
         # If this happens, retries should be handled manually, with exponential backoff (but limited to the
         # time it takes to fill the offending bucket from empty to full).
+
+
+if __name__ == '__main__':
+    main()
