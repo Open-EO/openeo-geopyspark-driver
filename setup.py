d83d18894f121ef1f5e94efb90d64edc745a4b85
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2021-12-21 11:46:22 +0100

    use pyspark 3.2.0 for python 3.8

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=[
         'openeo>=0.9.0',
         'openeo_driver>=0.14.9.dev',
-        'pyspark==3.1.1; python_version>="3.8"',
+        'pyspark==3.2.0; python_version>="3.8"',
         'pyspark>=2.3.1,<2.4.0; python_version<"3.8"',
         'geopyspark==0.4.7+openeo',
         'py4j',
