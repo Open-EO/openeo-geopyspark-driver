1eb05be56dfd1999192fcd2830209be641796ac8
Jan Van den bosch <jan@bossie.org>
2022-01-19 11:29:47 +0100

    unit test fix: title is not required in list jobs response
    was dropped by introduction of openeo_driver.backend.BatchJobMetadata.to_api_dict
    https://openeo.org/documentation/1.0/developers/api/reference.html#operation/list-jobs

--- tests/test_views.py
+++ tests/test_views.py
@@ -388,7 +388,7 @@ class TestBatchJobs:
             created = "created" if api.api_version_compare.at_least("1.0.0") else "submitted"
             assert result == {
                 "jobs": [
-                    {"id": job_id, "status": created, created: "2020-04-20T16:04:03Z", "title": "Dummy"},
+                    {"id": job_id, "status": created, created: "2020-04-20T16:04:03Z"},
                 ],
                 "links": []
             }
