0bb254b3993e26d0fc4f390ecf4e30b730ce0444
Jeroen Dries <jeroen.dries@vito.be>
2022-01-21 10:39:19 +0100

    create temp output dir in working directory

--- openeogeotrellis/geopysparkdatacube.py
+++ openeogeotrellis/geopysparkdatacube.py
@@ -1256,7 +1256,7 @@ class GeopysparkDataCube(DriverDataCube):
 
             wrapped = self._get_jvm().org.openeo.geotrellis.OpenEOProcesses().wrapCube(scala_data_cube)
             wrapped.openEOMetadata().setBandNames(bandNames)
-            temp_output = tempfile.mkdtemp(prefix="timeseries_",suffix="_csv")
+            temp_output = tempfile.mkdtemp(prefix="timeseries_", suffix="_csv", dir=pathlib.Path(".").resolve())
             self._compute_stats_geotrellis().compute_generic_timeseries_from_datacube(
                 func,
                 wrapped,
