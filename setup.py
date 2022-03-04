557e29a4104d8149bc9c0afa7e997b84b3a677fd
Jan Van den bosch <jan@bossie.org>
2022-03-04 17:10:52 +0100

    EP-4140: handle "raster:bands" for DEM layers

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=tests_require,
     install_requires=[
-        'openeo>=0.9.3a1.*',
+        'openeo>=0.9.3a2.*',
         'openeo_driver>=0.17.0.dev',
         'pyspark==3.2.0; python_version>="3.8"',
         'pyspark>=2.3.1,<2.4.0; python_version<"3.8"',
