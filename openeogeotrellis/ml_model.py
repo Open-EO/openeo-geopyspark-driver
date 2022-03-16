371debcf698cf638b3bc04e37d0cf0acf0d228b3
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2022-03-16 16:40:57 +0100

    EP-3778: implement create_flask_response method for MlModel

--- openeogeotrellis/ml_model.py
+++ openeogeotrellis/ml_model.py
@@ -1,11 +1,12 @@
 import pathlib
 from typing import Dict
+from flask import Response, jsonify
 from openeo_driver.save_result import SaveResult
 import geopyspark as gps
 from pyspark.mllib.util import JavaSaveable
 
-
 class MLModel(SaveResult):
+
     def __init__(self, model: JavaSaveable):
         super().__init__(format='standard', options={})
         self._model = model
@@ -18,8 +19,11 @@ class MLModel(SaveResult):
         """
         directory = pathlib.Path(directory).parent
         filename = str(pathlib.Path(directory) / "mlmodel.model")
-        self._model.save(gps.get_spark_context() , filename)
+        self._model.save(gps.get_spark_context(), filename)
         return {filename:{"href":filename}}
 
     def save_ml_model(self, directory: str) -> Dict:
         return self.write_assets(directory)
+
+    def create_flask_response(self) -> Response:
+        return jsonify({})
