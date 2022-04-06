dc52ac40e22cde55999820baadc58788e2d07790
Jan Van den bosch <jan@bossie.org>
2022-04-06 14:35:15 +0200

    support access_token requests in rlguard_adapter
    https://github.com/Open-EO/openeo-geotrellis-extensions/issues/40

--- openeogeotrellis/sentinel_hub/rlguard_adapter.py
+++ openeogeotrellis/sentinel_hub/rlguard_adapter.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import traceback
 from sys import argv
+from typing import Optional
 
 import kazoo.client
 
@@ -11,36 +12,54 @@ from openeogeotrellis.utils import zk_client
 from rlguard import apply_for_request, calculate_processing_units, OutputFormat, SyncerDownException
 from rlguard.repository import ZooKeeperRepository
 
+REQUEST_DELAY = 'delay'
+REQUEST_ACCESS_TOKEN = 'access_token'
+
 logging.basicConfig(level=logging.DEBUG)
 kazoo.client.log.setLevel(logging.WARNING)
 
 
-def request(request_params) -> (float, float):
+def zookeeper_repository(zk) -> ZooKeeperRepository:
+    return ZooKeeperRepository(zk, key_base="/openeo/rlguard")
+
+
+def calculate_delay(request_params) -> float:
     request_params['output_format'] = OutputFormat(request_params['output_format'])
 
     pu = calculate_processing_units(**request_params)
 
     with zk_client() as zk:
-        zookeeper_repository = ZooKeeperRepository(zk, key_base="/openeo/rlguard")
-        delay = apply_for_request(pu, zookeeper_repository)
+        delay = apply_for_request(pu, zookeeper_repository(zk))
 
-        return pu, delay
+    return delay
+
+
+def fetch_access_token() -> Optional[dict]:
+    with zk_client() as zk:
+        return zookeeper_repository(zk).get_access_token()
 
 
 def main():
     if len(argv) < 2:
-        print(f"Usage: {argv[0]} <request params JSON>", file=sys.stderr)
+        print(f"Usage: {argv[0]} <request JSON>", file=sys.stderr)
         exit(1)
 
-    request_params = json.loads(argv[1])
+    request = json.loads(argv[1])
+    request_id = request.get('request_id')
 
-    try:
-        pu, delay = request(request_params)
-        print(json.dumps({'processing_units': pu, 'delay_s': delay}))
-    except SyncerDownException:
-        print(json.dumps({'error': traceback.format_exc()}))
-        # If this happens, retries should be handled manually, with exponential backoff (but limited to the
-        # time it takes to fill the offending bucket from empty to full).
+    if request_id in [REQUEST_DELAY, None]:
+        try:
+            request_params = request['arguments'] if request_id else request
+            delay = calculate_delay(request_params)
+            print(json.dumps({'delay_s': delay}))
+        except SyncerDownException:
+            print(json.dumps({'error': traceback.format_exc()}))
+            # If this happens, retries should be handled manually, with exponential backoff (but limited to the
+            # time it takes to fill the offending bucket from empty to full).
+    elif request_id == REQUEST_ACCESS_TOKEN:
+        print(json.dumps({'access_token': fetch_access_token()}))
+    else:
+        raise ValueError(f'unsupported request_id "{request_id}"')
 
 
 if __name__ == '__main__':
