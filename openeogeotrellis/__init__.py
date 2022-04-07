0c2cb30996d58d5239d8b0a19839e6e783ce4456
Stefaan Lippens <stefaan.lippens@vito.be>
2022-04-07 17:09:56 +0200

    use verbose version (if available) for `backend_version` in capabilities doc

--- openeogeotrellis/__init__.py
+++ openeogeotrellis/__init__.py
@@ -2,6 +2,10 @@ from openeogeotrellis._version import __version__
 
 
 def get_backend_version() -> str:
-    return __version__
-
-
+    try:
+        import importlib.metadata
+        # also see issue #138 (distribution name is "openeo-geopyspark")
+        return importlib.metadata.version("openeo-geopyspark")
+    except Exception:
+        # Fallback
+        return __version__
