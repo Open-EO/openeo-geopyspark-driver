7b4e3f5b5b67e70c43f3431579457335c61bfbc6
Stefaan Lippens <stefaan.lippens@vito.be>
2022-04-07 18:00:48 +0200

    flask_config fix

--- openeogeotrellis/deploy/flask_config.py
+++ openeogeotrellis/deploy/flask_config.py
@@ -13,7 +13,7 @@ OPENEO_BACKEND_DEPLOY_METADATA = build_backend_deploy_metadata(
         "geopyspark",
     ]
     # TODO: add version info about geotrellis-extensions jar?
-),
-backend_version = get_backend_version(),
+)
+backend_version = get_backend_version()
 
 MAX_CONTENT_LENGTH = 1024 * 1024  # bytes
