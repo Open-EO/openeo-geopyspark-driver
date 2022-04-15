c29e2e908795f01580a69a6a8a3e246d1e63f82a
Jan Van den bosch <jan@bossie.org>
2022-04-15 14:08:41 +0200

    upcoming 0.18 comes with some backwards incompatibility
    https://github.com/openEOPlatform/architecture-docs/issues/42

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     tests_require=tests_require,
     install_requires=[
         'openeo>=0.9.3a2.*',
-        'openeo_driver>=0.17.10.dev',
+        'openeo_driver>=0.17.10.dev,<0.18',
         'pyspark==3.2.0; python_version>="3.8"',
         'pyspark>=2.3.1,<2.4.0; python_version<"3.8"',
         'geopyspark==0.4.7+openeo',
