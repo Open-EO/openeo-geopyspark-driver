bb11241bae48c6ea5a08c1da62204a85ef40b740
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2022-05-04 14:46:23 +0200

    add load_ml_model test for Catboost

--- tests/test_ml_model.py
+++ tests/test_ml_model.py
@@ -5,8 +5,10 @@ from typing import List
 from unittest import TestCase
 from unittest.mock import patch
 
+import mock
 import pytest
 import shapely.geometry
+from py4j.java_gateway import JavaObject
 from shapely.geometry import GeometryCollection, Point
 from openeo_driver.utils import EvalEnv
 from pyspark.mllib.tree import RandomForestModel
@@ -49,6 +51,28 @@ class MockResponse:
         return self.data
 
 
+def mocked_requests_get_catboost(*args, **kwargs):
+    item_url = 'https://openeo-test.vito.be/openeo/1.1.0/jobs/1234/results/items/ml_model_metadata.json'
+    asset_url = "https://openeo-test.vito.be/openeo/1.1.0/jobs/1234/results/assets/catboost_model.tar.gz"
+    if args[0] == item_url:
+        metadata = GeopySparkCatBoostModel(None).get_model_metadata("./")
+        metadata["assets"]["model"]["href"] = asset_url
+        return MockResponse(metadata, 200)
+    elif args[0] == asset_url:
+        source_path = Path("tests/data/catboost_model.tar.gz")
+        with open(source_path, 'rb') as f:
+            model = f.read()
+        return MockResponse(model, 200)
+    return MockResponse(None, 404)
+
+
+@mock.patch('openeogeotrellis.backend.requests.get', side_effect=mocked_requests_get_catboost)
+def test_load_ml_model_for_catboost(mock_get, backend_implementation):
+    request_url = "https://openeo-test.vito.be/openeo/1.1.0/jobs/1234/results/items/ml_model_metadata.json"
+    catboost_model = backend_implementation.load_ml_model(request_url)
+    assert isinstance(catboost_model, JavaObject)
+
+
 class TestFitClassRandomForestFlow(TestCase):
     def setUp(self):
         self.tmp_dir = tempfile.TemporaryDirectory("openeo-pydrvr-pytest-")
@@ -57,15 +81,14 @@ class TestFitClassRandomForestFlow(TestCase):
         self.addCleanup(self.patcher .stop)
 
     def mocked_requests_get_flow(self, *args, **kwargs):
-        collection_url = 'https://openeo-test.vito.be/openeo/1.1.0/jobs/1234/results/items/ml_model_metadata.json'
-        item_url = "https://openeo-test.vito.be/openeo/1.1.0/jobs/1234/results/assets/randomforest.model.tar.gz"
-        file_name = item_url.split('/')[-1]
-        if args[0] == collection_url:
-            metadata = GeopySparkMLModel(None, 'randomforest.model').get_model_metadata("./")
-            metadata["assets"]["model"][
-                "href"] = item_url
+        item_url = 'https://openeo-test.vito.be/openeo/1.1.0/jobs/1234/results/items/ml_model_metadata.json'
+        asset_url = "https://openeo-test.vito.be/openeo/1.1.0/jobs/1234/results/assets/randomforest.model.tar.gz"
+        file_name = asset_url.split('/')[-1]
+        if args[0] == item_url:
+            metadata = GeopySparkRandomForestModel(None).get_model_metadata("./")
+            metadata["assets"]["model"]["href"] = asset_url
             return MockResponse(metadata, 200)
-        elif args[0] == item_url:
+        elif args[0] == asset_url:
             source_path = Path(str(self.tmp_dir.name) + "/" + file_name)
             with open(source_path, 'rb') as f:
                 model = f.read()
@@ -93,7 +116,7 @@ class TestFitClassRandomForestFlow(TestCase):
         cube_xyb = cube_xybt.reduce_dimension(mean_reducer, 't', EvalEnv())
         predictors: AggregateSpatialVectorCube = cube_xyb.aggregate_spatial(geometries, mean_reducer, "bands")
         # 2. Fit model.
-        result: GeopySparkMLModel = predictors.fit_class_random_forest(FEATURE_COLLECTION_1, num_trees=3, seed=42)
+        result: GeopySparkRandomForestModel = predictors.fit_class_random_forest(FEATURE_COLLECTION_1, num_trees=3, seed=42)
         assert (predictors.prepare_for_json() == [[1.0, 2.0], [1.0, 2.0]])
         assert (result.get_model().predict([2.0, 2.0]) == 5)
         # 3. Save model.
@@ -119,7 +142,7 @@ def test_fit_class_random_forest_results():
     }
     # 2. Fit model.
     num_trees = 3
-    result: GeopySparkMLModel = predictors.fit_class_random_forest(target, num_trees=num_trees, seed=42)
+    result: GeopySparkRandomForestModel = predictors.fit_class_random_forest(target, num_trees=num_trees, seed=42)
     # 3. Test model.
     model: RandomForestModel = result.get_model()
     assert(model.numTrees() == num_trees)
