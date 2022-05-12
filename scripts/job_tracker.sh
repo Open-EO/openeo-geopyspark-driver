89bc406564fad150b846cb38bbb786182b746e81
Jan Van den bosch <jan@bossie.org>
2022-05-12 15:49:03 +0200

    fix hanging job_tracker process #134

--- scripts/job_tracker.sh
+++ scripts/job_tracker.sh
@@ -11,4 +11,7 @@ export SPARK_HOME="/opt/spark3_2_0"
 export PYTHONPATH="venv/lib/python3.8/site-packages:${SPARK_HOME}/python"
 export HADOOP_CONF_DIR="/etc/hadoop/conf"
 
+export KRB5CCNAME=/tmp/krb5cc_openeo
+kinit -kt openeo-deploy/mep/openeo.keytab openeo@VGT.VITO.BE
+
 python -m openeogeotrellis.job_tracker
