7046d68aef6cdad9e210eb398e5c3aae25751868
Stefaan Lippens <stefaan.lippens@vito.be>
2022-06-01 16:00:46 +0200

    fixup! GDD-2107 add sender to Traefik loadBalancer healthCheck

--- tests/test_traefik.py
+++ tests/test_traefik.py
@@ -15,7 +15,7 @@ def test_add_load_balanced_server():
         "/traefik/http/routers/openeo-test/rule": b"Host(`openeo-dev.vgt.vito.be`,`openeo-dev.vito.be`) && PathPrefix(`/openeo`,`/.well-known/openeo`)",
         "/traefik/http/routers/openeo-test/priority": b"100",
         "/traefik/http/services/openeo-test/loadBalancer/servers/openeo-test-01/url": b"http://10.0.0.0:123",
-        "/traefik/http/services/openeo-test/loadBalancer/healthCheck/path": b"/openeo/1.0/health",
+        "/traefik/http/services/openeo-test/loadBalancer/healthCheck/path": b"/openeo/1.0/health?from=TraefikLoadBalancer",
         "/traefik/http/services/openeo-test/loadBalancer/healthCheck/interval": b"60s",
         "/traefik/http/services/openeo-test/loadBalancer/healthCheck/timeout": b"20s",
     }
