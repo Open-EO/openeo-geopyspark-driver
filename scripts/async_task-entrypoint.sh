a4209db2cc8c1323dab6c25b8d1399024f6e77d1
Jan Van den bosch <jan@bossie.org>
2022-08-01 13:45:32 +0200

    added missing parts wrt/ batch job submission from async_task #174

--- scripts/async_task-entrypoint.sh
+++ scripts/async_task-entrypoint.sh
@@ -4,8 +4,8 @@
 
 set -eo pipefail
 
-if [ "$#" -lt 5 ]; then
-    >&2 echo "Usage: $0 <task JSON> <batch jobs Zookeeper root path> <Hadoop classpath> <AWS access key ID> <AWS secret access key>"
+if [ "$#" -lt 6 ]; then
+    >&2 echo "Usage: $0 <task JSON> <batch jobs Zookeeper root path> <Hadoop classpath> <AWS access key ID> <AWS secret access key> <batch job Docker image>"
     exit 1
 fi
 
@@ -14,19 +14,30 @@ if [ -z "${SPARK_HOME}" ]; then
     exit 1
 fi
 
+if [ -z "${HADOOP_CONF_DIR}" ]; then
+    >&2 echo "Environment variable HADOOP_CONF_DIR is not set"
+    exit 1
+fi
+
 task_json=$1
 batch_jobs_zookeeper_root_path=$2
 hadoop_classpath=$3
 aws_access_key_id=$4
 aws_secret_access_key=$5
+batch_job_docker_image=$6
 
 export PYTHONPATH="/opt/venv/lib64/python3.8/site-packages"
+export OPENEO_CATALOG_FILES="layercatalog.json"
 export BATCH_JOBS_ZOOKEEPER_ROOT_PATH=$batch_jobs_zookeeper_root_path
 export AWS_REGION="eu-central-1"
 export AWS_ACCESS_KEY_ID=$aws_access_key_id
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
+export OPENEO_SPARK_SUBMIT_PY_FILES="$(python3 /opt/get-py-files.py)"
+export YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$batch_job_docker_image
 
-classpath="geotrellis-extensions-static.jar:$(find $SPARK_HOME/jars -name '*.jar' | tr '\n' ':'):$hadoop_classpath"
+kinit -kt openeo.keytab openeo@VGT.VITO.BE
+
+classpath="geotrellis-extensions-static.jar:openeo-logging-static.jar:$(find $SPARK_HOME/jars -name '*.jar' | tr '\n' ':'):$hadoop_classpath"
 
 python3 -m openeogeotrellis.async_task --py4j-classpath "$classpath" --py4j-jarpath "venv/share/py4j/py4j0.10.9.2.jar" --task "$task_json"
 
