fda7af78b7cb1c72987c227c4c134375caf0e637
Jan Van den bosch <jan@bossie.org>
2022-09-15 09:48:43 +0200

    log data and code as separate fields
    https://github.com/Open-EO/openeo-python-driver/issues/124

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=tests_require,
     install_requires=[
-        'openeo>=0.9.3a2.*',
+        'openeo>=0.12.1a3.*',
         'openeo_driver>=0.20.4.dev',
         'pyspark==3.2.2; python_version>="3.8"',
         'pyspark>=2.3.1,<2.4.0; python_version<"3.8"',
