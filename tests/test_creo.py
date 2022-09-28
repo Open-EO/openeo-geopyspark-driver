46b991c218db5a1b51e73460369f6f25044a2205
Stefaan Lippens <stefaan.lippens@vito.be>
2022-09-28 10:29:54 +0200

    test_creo: simplify and harden

--- tests/test_creo.py
+++ tests/test_creo.py
@@ -1,17 +1,11 @@
-import unittest
 import datetime
 
 from openeogeotrellis.catalogs.creo import CreoCatalogClient
 
 
-class TestCreo(unittest.TestCase):
-
-    def setUp(self):
-        self.l2a_catalog = CreoCatalogClient(mission="Sentinel2", level="LEVEL2A")
-
-    def test_creo_catalog(self):
-        date = datetime.date(2018, 4, 1)
-
-        results = self.l2a_catalog.query_product_paths(date, date, ulx=5, uly=51.1, brx=5.1, bry=51)
-
-        assert 'S2A_MSIL2A_20180401T105031_N0207_R051_T31UFS_20180401T144530.SAFE' in results[0]
+def test_creo_catalog():
+    date = datetime.date(2018, 4, 1)
+    l2a_catalog = CreoCatalogClient(mission="Sentinel2", level="LEVEL2A")
+    results = l2a_catalog.query_product_paths(date, date, ulx=5, uly=51.1, brx=5.1, bry=51)
+    filenames = [r.split("/")[-1] for r in results]
+    assert "S2A_MSIL2A_20180401T105031_N0207_R051_T31UFS_20180401T144530.SAFE" in filenames
