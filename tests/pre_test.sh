e3c33aaa677b2f48684438e8ba9cafac4ed1b18b
Jan Van den bosch <jan@bossie.org>
2022-11-10 16:09:09 +0100

    add missing openeo-logging jar for tests
    https://github.com/Open-EO/openeo-geotrellis-extensions/issues/64
    
        def after_request(self):
            sc = SparkContext.getOrCreate()
            jvm = sc._gateway.jvm
    
    >       for mdc_key in [jvm.org.openeo.logging.JsonLayout.RequestId(), jvm.org.openeo.logging.JsonLayout.UserId()]:
    E       TypeError: 'JavaPackage' object is not callable

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -4,6 +4,7 @@ runuser jenkins -c '
 mkdir -p jars
 cd jars
 rm *.jar
-curl -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.3.0_2.12-SNAPSHOT/geotrellis-extensions-2.3.0_2.12-SNAPSHOT.jar
-curl -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
+curl -f -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/geotrellis-extensions/2.3.0_2.12-SNAPSHOT/geotrellis-extensions-2.3.0_2.12-SNAPSHOT.jar
+curl -f -L -O -C - https://artifactory.vgt.vito.be/libs-snapshot-public/org/openeo/openeo-logging/2.3.0_2.12-SNAPSHOT/openeo-logging-2.3.0_2.12-SNAPSHOT.jar
+curl -f -L -O -C - https://artifactory.vgt.vito.be/auxdata-public/openeo/geotrellis-backend-assembly-0.4.6-openeo_2.12.jar
 cd ..'
