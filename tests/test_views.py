058e99f599427694569951b0f7a9dd7897f221c0
Jan Van den bosch <jan@bossie.org>
2022-11-30 07:14:08 +0100

    updated failing test for Spark job kill #278

--- tests/test_views.py
+++ tests/test_views.py
@@ -600,14 +600,19 @@ class TestBatchJobs:
                 res.assert_status_code(204)
                 run.assert_called_once()
                 command = run.call_args[0][0]
-                assert command == [ 'curl',
-                                    '--negotiate',
-                                    '-u',
-                                    ':',
-                                    '--insecure',
-                                    '-X',
-                                    'PUT',
-                                    '-d',
-                                    '{"state": "KILLED"}',
-                                    'https://epod-master1.vgt.vito.be:8090/ws/v1/cluster/apps/application_1587387643572_0842']
-
+                assert command == [
+                    'curl',
+                    '--location-trusted',
+                    '--fail',
+                    '--negotiate',
+                    '-u',
+                    ':',
+                    '--insecure',
+                    '-X',
+                    'PUT',
+                    '-H',
+                    'Content-Type: application/json',
+                    '-d',
+                    '{"state": "KILLED"}',
+                    'https://epod-master1.vgt.vito.be:8090/ws/v1/cluster/apps/application_1587387643572_0842/state'
+                ]
