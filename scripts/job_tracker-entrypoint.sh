5421da3ca2e5d00dfb6e40ed858ac3bd06a005e2
Stefaan Lippens <stefaan.lippens@vito.be>
2023-01-23 15:37:02 +0100

    Fix openeo.keytab location in job_tracker-entrypoint
    related to #236 #272 #317

--- scripts/job_tracker-entrypoint.sh
+++ scripts/job_tracker-entrypoint.sh
@@ -9,11 +9,13 @@ fi
 
 batch_jobs_zookeeper_root_path=$1
 async_task_handler_env=$2
+# Note: this `./openeo.keytab` is expected to be mounted in docker container by NiFi processor.
+keytab="openeo.keytab"
 
-kinit -kt openeo.keytab openeo@VGT.VITO.BE
+kinit -kt $keytab openeo@VGT.VITO.BE
 
 export BATCH_JOBS_ZOOKEEPER_ROOT_PATH=$batch_jobs_zookeeper_root_path
 export ASYNC_TASK_HANDLER_ENV=$async_task_handler_env
 export PYARROW_IGNORE_TIMEZONE=1
 
-/opt/venv/bin/python -m openeogeotrellis.job_tracker
+/opt/venv/bin/python -m openeogeotrellis.job_tracker --keytab "$keytab"
