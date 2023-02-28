e6677d35dd3a51615f1b76afaf8487fe7fa57e56
Stefaan Lippens <stefaan.lippens@vito.be>
2023-02-28 11:34:51 +0100

    Fix "invalid escape sequence \/" warnings in test_load_collection tests

--- tests/test_load_collection.py
+++ tests/test_load_collection.py
@@ -355,11 +355,23 @@ def test_load_collection_common_name_by_missing_products(
     assert collection.metadata.get('id') == expected_source
 
 
-def test_load_disk_collection_pyramid(imagecollection_with_two_bands_and_three_dates,backend_implementation,tmp_path):
-    out = imagecollection_with_two_bands_and_three_dates.write_assets(filename=tmp_path/"out.tif",format="GTiff",format_options=dict(batch_mode=True))
-    #example output path: /tmp/pytest-of-driesj/pytest-1/test_load_disk_collection0/openEO_2017-10-25Z.tif
-    cube = backend_implementation.load_disk_data(format="GTiff",glob_pattern=str(tmp_path/"openEO_*.tif"),options=dict(date_regex=".*\/openEO_(\d{4})-(\d{2})-(\d{2})Z.tif"),load_params=LoadParameters(),env=EvalEnv())
-    cube = cube.rename_labels("bands",["band1","bands"])
+def test_load_disk_collection_pyramid(
+    imagecollection_with_two_bands_and_three_dates, backend_implementation, tmp_path
+):
+    out = imagecollection_with_two_bands_and_three_dates.write_assets(
+        filename=tmp_path / "out.tif",
+        format="GTiff",
+        format_options=dict(batch_mode=True),
+    )
+    # example output path: /tmp/pytest-of-driesj/pytest-1/test_load_disk_collection0/openEO_2017-10-25Z.tif
+    cube = backend_implementation.load_disk_data(
+        format="GTiff",
+        glob_pattern=str(tmp_path / "openEO_*.tif"),
+        options=dict(date_regex=r".*\/openEO_(\d{4})-(\d{2})-(\d{2})Z.tif"),
+        load_params=LoadParameters(),
+        env=EvalEnv(),
+    )
+    cube = cube.rename_labels("bands", ["band1", "bands"])
 
     assert len(cube.metadata.spatial_dimensions) == 2
     assert len(cube.pyramid.levels) == 2
@@ -373,8 +385,14 @@ def test_load_disk_collection_batch(imagecollection_with_two_bands_and_three_dat
     load_params.spatial_extent = dict(west=2,east=3,south=1,north=2)
     env = EvalEnv(dict(pyramid_levels="1"))
 
-    cube = backend_implementation.load_disk_data(format="GTiff", glob_pattern=str(tmp_path/"openEO_*.tif"), options=dict(date_regex=".*\/openEO_(\d{4})-(\d{2})-(\d{2})Z.tif"), load_params=load_params, env=env)
-    cube = cube.rename_labels("bands",["band1","bands"])
+    cube = backend_implementation.load_disk_data(
+        format="GTiff",
+        glob_pattern=str(tmp_path / "openEO_*.tif"),
+        options=dict(date_regex=r".*\/openEO_(\d{4})-(\d{2})-(\d{2})Z.tif"),
+        load_params=load_params,
+        env=env,
+    )
+    cube = cube.rename_labels("bands", ["band1", "bands"])
 
     assert len(cube.metadata.spatial_dimensions) == 2
     assert len(cube.pyramid.levels)==1
