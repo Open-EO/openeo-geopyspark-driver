3b122f6924013808fd13ed71cd343a1c2e815556
Jan Van den bosch <jan@bossie.org>
2023-04-04 11:45:42 +0200

    remove log4j.properties superseded by log4j2.xml
    https://github.com/Open-EO/openeo-geotrellis-extensions/issues/109

--- setup.py
+++ setup.py
@@ -40,12 +40,10 @@ setup(
             # TODO: make these config files real "package_data" so that they can be managed/found more easily in different contexts
             "scripts/submit_batch_job_log4j.properties",
             "scripts/submit_batch_job_log4j2.xml",
-            "scripts/batch_job_log4j.properties",
             "scripts/batch_job_log4j2.xml",
             "scripts/cleaner-entrypoint.sh",
             "scripts/job_tracker-entrypoint.sh",
             "scripts/async_task-entrypoint.sh",
-            "scripts/async_task_log4j.properties",
             "scripts/async_task_log4j2.xml"
         ]),
     ],
