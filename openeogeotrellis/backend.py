32a13072e941f417d8cbcbb8f10f4640117a3642
Stefaan Lippens <stefaan.lippens@vito.be>
2023-06-13 12:25:10 +0200

    Issue #399 fail harder when EJR from env is required but doesn't work
    better logging of what is wrong

--- openeogeotrellis/backend.py
+++ openeogeotrellis/backend.py
@@ -1106,9 +1106,13 @@ def get_elastic_job_registry(
         # Get credentials from env (preferably) or vault (as fallback).
         ejr_creds = ElasticJobRegistryCredentials.from_env(strict=False)
         if not ejr_creds:
-            # TODO: eliminate dependency on Vault here (i.e.: always use env vars to get creds)
-            vault = Vault(config.vault_addr, requests_session=requests_session)
-            ejr_creds = vault.get_elastic_job_registry_credentials()
+            if config.ejr_credentials_vault_path:
+                # TODO: eliminate dependency on Vault here (i.e.: always use env vars to get creds)
+                vault = Vault(config.vault_addr, requests_session=requests_session)
+                ejr_creds = vault.get_elastic_job_registry_credentials()
+            else:
+                # Fail harder
+                ejr_creds = ElasticJobRegistryCredentials.from_env(strict=True)
         job_registry.setup_auth_oidc_client_credentials(credentials=ejr_creds)
         job_registry.health_check(log=True)
         return job_registry
