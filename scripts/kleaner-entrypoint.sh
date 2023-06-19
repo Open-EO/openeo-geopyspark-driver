09705a5a3b2dc8c84938748d2e8254422835d54e
Jan Van den bosch <jan@bossie.org>
2023-06-19 09:51:07 +0200

    fail fast for missing environment variables
    https://github.com/eu-cdse/openeo-cdse-infra/issues/5

--- scripts/kleaner-entrypoint.sh
+++ scripts/kleaner-entrypoint.sh
@@ -4,7 +4,28 @@
 
 set -eo pipefail
 
-# TODO: fail fast for missing environment variables
+fail_if_not_set () {
+  local -n envars=$1
+
+  for envar in "${envars[@]}"; do
+    if [ -z "${!envar}" ]; then
+      >&2 echo "Environment variable ${envar} is not set"
+      exit 1
+    fi
+  done
+}
+
+expected_envars=(
+"SPARK_HOME"
+"ZOOKEEPERNODES"
+"BATCH_JOBS_ZOOKEEPER_ROOT_PATH"
+"SWIFT_URL"
+"AWS_ACCESS_KEY_ID"
+"AWS_SECRET_ACCESS_KEY"
+)
+fail_if_not_set expected_envars
+
+export KUBE="true"
 
 # TODO: wasn't there a function in py4j to find this jar?
 py4j_jarpath="$(find venv/share/py4j -name 'py4j*.jar')"
