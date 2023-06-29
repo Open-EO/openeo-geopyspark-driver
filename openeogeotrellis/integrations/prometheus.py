1134af3eb6d5d454ba36226fafc44788a630c549
Jan Van den bosch <jan@bossie.org>
2023-06-29 14:46:47 +0200

    missing metrics are not exceptional for short jobs #396
    Traceback (most recent call last):
      File "/opt/openeo/lib/python3.8/site-packages/openeogeotrellis/job_tracker_v2.py", line 285, in _get_usage
        byte_seconds = prometheus.get_memory_usage(application_id, application_duration_s)
      File "/opt/openeo/lib/python3.8/site-packages/openeogeotrellis/integrations/prometheus.py", line 44, in get_memory_usage
        return self._query_for_value(query, at)
      File "/opt/openeo/lib/python3.8/site-packages/openeogeotrellis/integrations/prometheus.py", line 22, in _query_for_value
        value = float(resp.json()['data']['result'][0]['value'][1])
    IndexError: list index out of range

--- openeogeotrellis/integrations/prometheus.py
+++ openeogeotrellis/integrations/prometheus.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import requests
 
 
@@ -8,7 +10,7 @@ class Prometheus:
         """Point this to e.g. http://example.org:9090/api/v1."""
         self._api_endpoint = api_endpoint
 
-    def _query_for_value(self, query: str, time: str = None) -> float:
+    def _query_for_float(self, query: str, time: str = None) -> Optional[float]:
         params = {
             'query': query
         }
@@ -18,30 +20,35 @@ class Prometheus:
 
         with requests.get(self._api_endpoint + "/query", params=params) as resp:
             resp.raise_for_status()
+            entity = resp.json()
 
-            value = float(resp.json()['data']['result'][0]['value'][1])
+        status = entity["status"]
+        if status == "error":
+            raise Exception(f"query {query} at {time} returned status {status}: {entity['error']}")
 
-        return value
+        results = entity['data']['result']
 
-    def get_cpu_usage(self, application_id: str, at: str = None) -> float:
+        return float(results[0]['value'][1]) if len(results) > 0 else None
+
+    def get_cpu_usage(self, application_id: str, at: str = None) -> Optional[float]:
         """Returns CPU usage in cpu-seconds."""
 
         query = f'sum(last_over_time(container_cpu_usage_seconds_total{{pod=~"{application_id}.+"}}[5d]))'
-        return self._query_for_value(query, at)
+        return self._query_for_float(query, at)
 
-    def get_network_received_usage(self, application_id: str, at: str = None) -> float:
+    def get_network_received_usage(self, application_id: str, at: str = None) -> Optional[float]:
         """Returns bytes received."""
 
         query = f'sum(last_over_time(container_network_receive_bytes_total{{pod=~"{application_id}-.+"}}[5d]))'
-        return self._query_for_value(query, at)
+        return self._query_for_float(query, at)
 
-    def get_memory_usage(self, application_id: str, application_duration_s: float, at: str = None) -> float:
+    def get_memory_usage(self, application_id: str, application_duration_s: float, at: str = None) -> Optional[float]:
         """Returns memory usage in byte-seconds."""
 
         # Prometheus doesn't expose this as a counter: do integration over time ourselves
         query = f'sum(avg_over_time(container_memory_usage_bytes{{pod=~"{application_id}-.+"}}[5d])) ' \
                 f'* {application_duration_s}'
-        return self._query_for_value(query, at)
+        return self._query_for_float(query, at)
 
 
 if __name__ == '__main__':
@@ -50,9 +57,11 @@ if __name__ == '__main__':
     application_id = 'job-90206ae556-df7ea45d'
     application_finish_time = "2023-06-22T11:30:46Z"
 
-    print(prometheus.get_cpu_usage(application_id=application_id, at=application_finish_time), "cpu-seconds")
-    print(prometheus
-          .get_memory_usage(application_id=application_id, application_duration_s=52, at=application_finish_time)
-          / 1024 / 1024, "mb-seconds")
-    print(prometheus.get_network_received_usage(
-        application_id=application_id, at=application_finish_time) / 1024 / 1024, "mb")
+    cpu_seconds = prometheus.get_cpu_usage(application_id=application_id, at=application_finish_time)
+    byte_seconds = prometheus.get_memory_usage(application_id=application_id, application_duration_s=52,
+                                               at=application_finish_time)
+    bytes_received = prometheus.get_network_received_usage(application_id=application_id, at=application_finish_time)
+
+    print(cpu_seconds if cpu_seconds is not None else "unknown", "cpu-seconds")
+    print(byte_seconds / 1024 / 1024 if byte_seconds is not None else "unknown", "mb-seconds")
+    print(bytes_received / 1024 / 1024 if bytes_received is not None else "unknown", "mb")
