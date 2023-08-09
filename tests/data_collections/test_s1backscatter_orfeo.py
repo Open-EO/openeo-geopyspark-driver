3b6bb85149acd733b894ef63d718cbb384b6f55a
Jeroen Dries <jeroen.dries@vito.be>
2023-08-09 19:50:24 +0200

    do not attempt to use COG's in the test because they don't exist

--- tests/data_collections/test_s1backscatter_orfeo.py
+++ tests/data_collections/test_s1backscatter_orfeo.py
@@ -402,6 +402,7 @@ class TestOrfeoPipeline:
                 elevation_model=elevation_model,
                 options={"dem_zoom_level": 6, "implementation_version": implementation_version, "debug": True},
             ),
+            properties={"COG":"FALSE"}
         )
         datacube = catalog.load_collection("Creodias-S1-Backscatter", load_params=load_params, env=EvalEnv())
 
