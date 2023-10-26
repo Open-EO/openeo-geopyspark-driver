a0717dd15f02c8528066eb03d1f20fb11105af9c
Stefaan Lippens <stefaan.lippens@vito.be>
2023-10-26 13:40:55 +0200

    make local_config a bit more usable

--- openeogeotrellis/deploy/local_config.py
+++ openeogeotrellis/deploy/local_config.py
@@ -1,7 +1,29 @@
+from openeo_driver.users.oidc import OidcProvider
 from openeogeotrellis.config import GpsBackendConfig
 
+
+oidc_default_client_egi = {
+    "id": "vito-default-client",
+    "grant_types": [
+        "urn:ietf:params:oauth:grant-type:device_code+pkce",
+        "refresh_token",
+    ],
+    "redirect_urls": [],
+}
+
+oidc_providers = [
+    OidcProvider(
+        id="egi",
+        issuer="https://aai.egi.eu/auth/realms/egi/",
+        scopes=["openid"],
+        title="EGI Check-in",
+        default_clients=[oidc_default_client_egi],
+    ),
+]
 config = GpsBackendConfig(
     id="gps-local",
     capabilities_title="Local GeoPySpark openEO Backend",
     capabilities_description="Local GeoPySpark openEO Backend",
+    oidc_providers=oidc_providers,
+    opensearch_enrich=False,
 )
