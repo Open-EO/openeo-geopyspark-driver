cd253b9c28441d4be81ff4fba0b07a4ebccfb735
Stefaan Lippens <stefaan.lippens@vito.be>
2023-11-28 11:48:36 +0100

    zookeeper_set.py: avoid toplevel logic

--- scripts/zookeeper_set.py
+++ scripts/zookeeper_set.py
@@ -12,11 +12,17 @@ def zk_set(hosts: str, path: str, value: bytes):
             _, stat = zk.get(path)
             zk.set(path, value, version = stat.version)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--servers", help="Comma separated list of zookeeper hosts/servers")
+    parser.add_argument(
+        "-p", "--path", default="/openeo/config/users/testuser/concurrent_pod_limit", help="Path to the node"
+    )
+    parser.add_argument("-v", "--value", help="Value to set")
+    args = parser.parse_args()
+    value_encoded = args.value.encode()
+    zk_set(args.hosts, args.path, value_encoded)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--servers", help = "Comma separated list of zookeeper hosts/servers")
-parser.add_argument("-p", "--path", default = "/openeo/config/users/testuser/concurrent_pod_limit", help = "Path to the node")
-parser.add_argument("-v", "--value", help = "Value to set")
-args = parser.parse_args()
-value_encoded = args.value.encode()
-zk_set(args.hosts, args.path, value_encoded)
+
+if __name__ == "__main__":
+    main()
