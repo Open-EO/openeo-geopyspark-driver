d87d1a7deecdb05744d8a3308087e815333bf9bb
JeroenVerstraelen <verstraelenjeroen@gmail.com>
2023-12-11 09:05:02 +0100

    add test_apply_neighborhood_overlap_jep_multiresolution #604

--- tests/test_apply_neighborhood.py
+++ tests/test_apply_neighborhood.py
@@ -5,6 +5,7 @@ import pytest
 from numpy.testing import assert_array_almost_equal
 
 from openeo_driver.utils import EvalEnv
+from .data import get_test_data_file
 
 
 def test_apply_neighborhood_no_overlap(imagecollection_with_two_bands_and_three_dates):
@@ -89,3 +90,36 @@ def test_apply_neighborhood_on_timeseries(imagecollection_with_two_bands_and_thr
     expected_result = np.power(2, input)
     print(expected_result)
     assert_array_almost_equal(expected_result, result_array)
+
+
+
+def test_apply_neighborhood_overlap_jep_multiresolution(imagecollection_with_two_bands_and_one_date_multiple_values):
+    imagecollection_with_two_bands_and_one_date_multiple_values.metadata.spatial_extent = None
+    imagecollection_with_two_bands_and_one_date_multiple_values.metadata.temporal_extent = None
+    file_name = get_test_data_file("udf_multiresolution.py")
+    with open(file_name, "r") as f:
+        udf_code = f.read()
+    udf_multiresolution = {
+        "udf_process": {
+            "process_id": "run_udf",
+            "arguments": {
+                "data": {
+                    "from_parameter": "data"
+                },
+                "udf": udf_code,
+                "runtime": "Python-Jep",
+            },
+            "result": True
+        },
+    }
+    the_date = datetime.datetime(2017, 9, 25, 11, 37)
+    # input_cells = imagecollection_with_two_bands_and_one_date_multiple_values.pyramid.levels[0].to_spatial_layer(the_date).stitch().cells
+    imagecollection_with_two_bands_and_one_date_multiple_values.save_result("input_multires.nc", "NetCDF")
+    result = imagecollection_with_two_bands_and_one_date_multiple_values.apply_neighborhood(
+        process=udf_multiresolution,
+        size=[{'dimension': 'x', 'unit': 'px', 'value': 32}, {'dimension': 'y', 'unit': 'px', 'value': 32}],
+        overlap=[{'dimension': 'x', 'unit': 'px', 'value': 8}, {'dimension': 'y', 'unit': 'px', 'value': 8}],
+        context={},
+        env=EvalEnv()
+    )
+    result.save_result("output_multires_fixed.nc", "NetCDF")
