ca2b1655cec549cf45b1e73ce1f668e13c77bfad
Jan Van den bosch <jan@bossie.org>
2024-01-18 15:27:01 +0100

    reference "released" openeo_driver #632

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     tests_require=tests_require,
     install_requires=[
         "openeo>=0.15.0",
-        "openeo_driver_phase_out_zkjobregistry>=0.81.0.dev",
+        "openeo_driver>=0.82.0.dev",
         'pyspark==3.3.1; python_version>="3.8"',
         'pyspark>=2.3.1,<2.4.0; python_version<"3.8"',
         'geopyspark==0.4.7+openeo',
