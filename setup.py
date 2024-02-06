3a56bcbe2b734563ea21bafe461046d41424e09e
Jan Van den bosch <jan@bossie.org>
2024-02-06 10:11:55 +0100

    drop obsolete rlguard-lib dependency #672 #216

--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ setup(
         'numbagg==0.1',
         "Bottleneck~=1.3.2",
         'python-json-logger',
-        'rlguard-lib>=0.0.6',  # TODO this currently depends on custom packaging, see https://github.com/sentinel-hub/rate-limiting-guard/issues/5 and https://jira.vito.be/browse/EP-4001
         'jep==4.1.1',
         'kafka-python==1.4.6',
         'deprecated>=1.2.12',
