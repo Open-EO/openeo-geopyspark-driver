90ae8c2eafb1309df1a0747979d32d82476141c5
JeroenVerstraelen <verstraelenjeroen@gmail.com>
2024-03-07 16:30:37 +0100

    Issue #663 use original scala band names in tests

--- tests/test_vectorcube.py
+++ tests/test_vectorcube.py
@@ -139,7 +139,7 @@ def test_aggregatespatialvectorcube_to_vectorcube(imagecollection_with_two_bands
     input_data: gpd.GeoDataFrame = aggregate_result._get_geodataframe()
     num_geometries = input_data.shape[0]
     assert input_data.shape == (num_geometries, 3)
-    assert input_data.columns.tolist() == ['geometry', 'avg_band_0_', 'avg_band_1_']
+    assert input_data.columns.tolist() == ['geometry', 'avg(band_0)', 'avg(band_1)']
 
     # Check geometries.
     input_regions: gpd.GeoSeries = input_data.geometry
@@ -152,7 +152,7 @@ def test_aggregatespatialvectorcube_to_vectorcube(imagecollection_with_two_bands
     cube: xarray.DataArray = output_vector_cube.get_cube()
     assert cube.dims == ('geometry', 'bands')
     assert cube.shape == (num_geometries, 2)
-    assert cube['bands'].values.tolist() == ['avg_band_0_', 'avg_band_1_']  # Only numeric bands, ids are strings.
+    assert cube['bands'].values.tolist() == ['avg(band_0)', 'avg(band_1)']  # Only numeric bands, ids are strings.
     assert cube.isel(geometry=0).values.tolist() == input_data.values[0,1:].tolist()
 
 
