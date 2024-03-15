5f266e8fafb41028a120098b34feb33d6df9f7b6
Emile Sonneveld <emile.sonneveld@vito.be>
2024-03-15 09:08:16 +0100

    Larger tile size in TestMultipleDates to avoid many partitions

--- tests/test_multiple_date.py
+++ tests/test_multiple_date.py
@@ -74,7 +74,7 @@ class TestMultipleDates(TestCase):
     rdd = SparkContext.getOrCreate().parallelize(layer)
 
     extent = {'xmin': 0.0, 'ymin': 0.0, 'xmax': 33.0, 'ymax': 33.0}
-    layout = {'layoutCols': 2, 'layoutRows': 2, 'tileCols': 5, 'tileRows': 5}
+    layout = {'layoutCols': 2, 'layoutRows': 2, 'tileCols': 254, 'tileRows': 254}
     metadata = {'cellType': 'float32ud-1.0',
                 'extent': extent,
                 'crs': '+proj=longlat +datum=WGS84 +no_defs ',
@@ -83,7 +83,7 @@ class TestMultipleDates(TestCase):
                     'maxKey': {'col': 1, 'row': 1, 'instant': _convert_to_unix_time(time_3)}},
                 'layoutDefinition': {
                     'extent': extent,
-                    'tileLayout': {'tileCols': 5, 'tileRows': 5, 'layoutCols': 2, 'layoutRows': 2}}}
+                    'tileLayout': layout.copy()}}
     collection_metadata = GeopysparkCubeMetadata({
         "cube:dimensions": {
             "t": {"type": "temporal"},
