f81e99aa8a17449b59ca955065186129fa1e4c51
Jan Van den bosch <jan@bossie.org>
2024-06-19 12:49:36 +0200

    add (intentionally failing) test
    https://github.com/eu-cdse/openeo-cdse-infra/issues/166

--- tests/test_job_registry.py
+++ tests/test_job_registry.py
@@ -187,6 +187,37 @@ class TestZkJobRegistry:
                 process={"process_graph": {"e": {"process_id": "e", "result": True}}},
             )
 
+    @pytest.mark.parametrize(
+        ["job_deleted_while_collecting", "expected_job_ids"],
+        [
+            (False, ["j123"]),  # regular case
+            (True, [])  # deleted in the meanwhile, just skip the job
+        ],
+    )
+    def test_get_running_jobs(self, zk_client, caplog, job_deleted_while_collecting, expected_job_ids):
+        # TODO: make zk_client.get_job() throw a NoNodeError for /openeo/jobs/ongoing/j123
+        zjr = ZkJobRegistry(zk_client=zk_client)
+
+        zjr.register(
+            job_id="j123", user_id="u456", api_version="1.2.3", specification={"process_graph": {"foo": "bar"}}
+        )
+        zjr.set_application_id(job_id="j123", user_id="u456", application_id="application_1718705245374_0056")
+
+        zk_client_get = zk_client.get
+
+        def side_effect(path):
+            if job_deleted_while_collecting:
+                raise kazoo.exceptions.NoNodeError
+
+            return zk_client_get(path)
+
+        with mock.patch.object(zk_client, "get", side_effect=side_effect):
+            jobs_to_track = zjr.get_running_jobs()
+            assert [job['job_id'] for job in jobs_to_track] == expected_job_ids
+
+        print(caplog.messages)
+        # TODO: capture logs and check for WARNING
+
 
 class TestInMemoryJobRegistry:
     @pytest.fixture(autouse=True)
