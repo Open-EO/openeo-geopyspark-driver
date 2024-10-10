fc6dc026b747e97398a4c93aaa48c908979b8474
Jan Van den bosch <jan@bossie.org>
2024-10-10 14:20:45 +0200

    try workaround for tiffset segfault in container
    https://github.com/Open-EO/openeo-geotrellis-extensions/issues/317

--- openeogeotrellis/integrations/tiffset.py
+++ openeogeotrellis/integrations/tiffset.py
@@ -1,8 +1,14 @@
 import subprocess
+import tempfile
 from pathlib import Path
 from typing import Union
 
 
 def embed_gdal_metadata(gdal_metadata_xml: str, geotiff_path: Union[Path, str]):
     # TODO: use gdal instead to avoid warnings or are they harmless?
-    subprocess.check_call(["tiffset", "-s", "42112", gdal_metadata_xml, str(geotiff_path)])
+    with tempfile.NamedTemporaryFile(prefix="GDALMetadata", suffix=".xml.tmp", mode="wt", encoding="ascii") as tmp:
+        # use temporary file to work around segfault in container
+        tmp.write(gdal_metadata_xml)
+        tmp.flush()
+
+        subprocess.check_call(["tiffset", "-sf", "42112", tmp.name, str(geotiff_path)])
