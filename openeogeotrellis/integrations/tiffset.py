a08da1bd1bcf174018b7afba056f3448920f3700
Jan Van den bosch <jan@bossie.org>
2024-10-10 16:43:52 +0200

    fix tests
    https://github.com/Open-EO/openeo-geotrellis-extensions/issues/317

--- openeogeotrellis/integrations/tiffset.py
+++ openeogeotrellis/integrations/tiffset.py
@@ -6,9 +6,9 @@ from typing import Union
 
 def embed_gdal_metadata(gdal_metadata_xml: str, geotiff_path: Union[Path, str]):
     # TODO: use gdal instead to avoid warnings or are they harmless?
-    with tempfile.NamedTemporaryFile(prefix="GDALMetadata", suffix=".xml.tmp", mode="wt", encoding="ascii") as tmp:
+    with tempfile.NamedTemporaryFile(prefix="GDALMetadata_", suffix=".xml.tmp", mode="wt", encoding="ascii") as tmp:
         # use temporary file to work around segfault in container
-        tmp.write(gdal_metadata_xml)
+        tmp.write(f"{gdal_metadata_xml}\n")
         tmp.flush()
 
         subprocess.check_call(["tiffset", "-sf", "42112", tmp.name, str(geotiff_path)])
