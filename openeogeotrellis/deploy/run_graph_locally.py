88ab283dde98209acbf47c5c4f10a1e92796ba85
Emile Sonneveld <emile.sonneveld@vito.be>
2024-11-25 09:00:54 +0100

    fix logfile in test_local_batch_job_example

--- openeogeotrellis/deploy/run_graph_locally.py
+++ openeogeotrellis/deploy/run_graph_locally.py
@@ -35,7 +35,7 @@ def run_graph_locally(process_graph, output_dir):
         output_dir / "collection.json",
     ]
     if os.path.exists(output_dir / "openeo.log"):
-        files += output_dir / "openeo.log"
+        files += [output_dir / "openeo.log"]
     with open(output_dir / JOB_METADATA_FILENAME) as f:
         j = json.load(f)
         files += [output_dir / asset["href"] for asset in j.get("links", [])]
