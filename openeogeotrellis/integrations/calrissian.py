1091dd9274d787c15a34caddfaa5d6a742196dbf
Stefaan Lippens <stefaan.lippens@vito.be>
2024-11-29 22:31:39 +0100

    Issue #936 add launch_cwl_job_and_wait

--- openeogeotrellis/integrations/calrissian.py
+++ openeogeotrellis/integrations/calrissian.py
@@ -1,10 +1,14 @@
+import logging
+import dataclasses
+import time
 from typing import Optional
 
+from openeo.util import ContextTimer
 from openeo_driver.utils import generate_unique_id
-import dataclasses
-
 from openeogeotrellis.config import get_backend_config
 
+
+_log = logging.getLogger(__name__)
 
 @dataclasses.dataclass(frozen=True)
 class VolumeInfo:
@@ -16,9 +20,9 @@ class VolumeInfo:
 
 def create_cwl_job_body(
     *,
-    # TODO: don't define a default namespace here
     namespace: Optional[str] = None,
     name: Optional[str] = None,
+    # TODO: arguments to set an actual CWL workflow and inputs
 ) -> "kubernetes.client.V1Job":
     import kubernetes.client
 
@@ -28,7 +32,7 @@ def create_cwl_job_body(
     if not namespace or not container_image:
         raise ValueError(f"Must be set: {namespace=}, {container_image=}")
 
-    # TODO: config for this
+    # TODO: config for this?
     security_context = kubernetes.client.V1SecurityContext(run_as_user=1000, run_as_group=1000)
 
     volumes = [
@@ -64,6 +68,29 @@ def create_cwl_job_body(
         "/calrissian/input-data/hello-input.yaml",
     ]
 
+    container = kubernetes.client.V1Container(
+        name="calrissian",
+        image=container_image,
+        security_context=security_context,
+        command=["calrissian"],
+        args=calrissian_arguments,
+        volume_mounts=[
+            kubernetes.client.V1VolumeMount(
+                name=v.name,
+                mount_path=v.mount_path,
+                read_only=v.read_only,
+            )
+            for v in volumes
+        ],
+        env=[
+            kubernetes.client.V1EnvVar(
+                name="CALRISSIAN_POD_NAME",
+                value_from=kubernetes.client.V1EnvVarSource(
+                    field_ref=kubernetes.client.V1ObjectFieldSelector(field_path="metadata.name")
+                ),
+            )
+        ],
+    )
     body = kubernetes.client.V1Job(
         metadata=kubernetes.client.V1ObjectMeta(
             name=name,
@@ -72,31 +99,7 @@ def create_cwl_job_body(
         spec=kubernetes.client.V1JobSpec(
             template=kubernetes.client.V1PodTemplateSpec(
                 spec=kubernetes.client.V1PodSpec(
-                    containers=[
-                        kubernetes.client.V1Container(
-                            name="calrissian",
-                            image=container_image,
-                            security_context=security_context,
-                            command=["calrissian"],
-                            args=calrissian_arguments,
-                            volume_mounts=[
-                                kubernetes.client.V1VolumeMount(
-                                    name=v.name,
-                                    mount_path=v.mount_path,
-                                    read_only=v.read_only,
-                                )
-                                for v in volumes
-                            ],
-                            env=[
-                                kubernetes.client.V1EnvVar(
-                                    name="CALRISSIAN_POD_NAME",
-                                    value_from=kubernetes.client.V1EnvVarSource(
-                                        field_ref=kubernetes.client.V1ObjectFieldSelector(field_path="metadata.name")
-                                    ),
-                                )
-                            ],
-                        )
-                    ],
+                    containers=[container],
                     restart_policy="Never",
                     volumes=[
                         kubernetes.client.V1Volume(
@@ -113,3 +116,48 @@ def create_cwl_job_body(
         ),
     )
     return body
+
+
+def launch_cwl_job_and_wait(
+    body: "kubernetes.client.V1Job",
+    *,
+    namespace: str,
+    sleep: float = 10,
+    timeout: float = 300,
+) -> "kubernetes.client.V1Job":
+    import kubernetes.client
+
+    k8s_batch = kubernetes.client.BatchV1Api()
+
+    # Launch job.
+    job: kubernetes.client.V1Job = k8s_batch.create_namespaced_job(
+        namespace=namespace,
+        body=body,
+    )
+    job_name = job.metadata.name
+    _log.info(
+        f"Created CWL job {job.metadata.name=} {job.metadata.namespace=} {job.metadata.creation_timestamp=} {job.metadata.uid=}"
+    )
+
+    # Track job status.
+    final_status = None
+    with ContextTimer() as timer:
+        while timer.elapsed() < timeout:
+            job: kubernetes.client.V1Job = k8s_batch.read_namespaced_job(name=job_name, namespace=namespace)
+            _log.info(f"CWL job {job_name=} {timer.elapsed()=:.2f} {job.status=}")
+            if job.status.conditions:
+                if any(c.type == "Failed" and c.status == "True" for c in job.status.conditions):
+                    final_status = "failed"
+                    break
+                elif any(c.type == "Complete" and c.status == "True" for c in job.status.conditions):
+                    final_status = "complete"
+                    break
+            time.sleep(sleep)
+
+    _log.info(f"CWL job {job_name=} {timer.elapsed()=:.2f} {final_status=}")
+    if not final_status:
+        raise TimeoutError(f"CWL Job {job_name} did not finish within {timeout}s")
+
+    # TODO: raise Exception if final status is "failed" too?
+
+    return job
