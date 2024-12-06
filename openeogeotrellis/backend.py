40bdca8d00ebe00af75b6345d8555ff4e642d786
Emile Sonneveld <emile.sonneveld@vito.be>
2024-12-06 09:34:37 +0100

    Pass TRY_SWIFT_STREAMING env var to Spark (read in scala code)

--- openeogeotrellis/backend.py
+++ openeogeotrellis/backend.py
@@ -2004,7 +2004,8 @@ class GpsBatchJobs(backend.BatchJobs):
                 profile=profile,
                 batch_scheduler=get_backend_config().batch_scheduler,
                 yunikorn_queue=get_backend_config().yunikorn_queue,
-                yunikorn_scheduling_timeout=get_backend_config().yunikorn_scheduling_timeout.rstrip()
+                yunikorn_scheduling_timeout=get_backend_config().yunikorn_scheduling_timeout.rstrip(),
+                try_swift_streaming=os.environ.get("TRY_SWIFT_STREAMING"),
             )
 
             if get_backend_config().fuse_mount_batchjob_s3_bucket:
