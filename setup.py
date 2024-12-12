cc1cca521ae8175271ede514a90612532156c986
Stefaan Lippens <stefaan.lippens@vito.be>
2024-12-12 12:45:00 +0100

    Update/annotate python-json-logger constraint

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
         'numbagg==0.1',
         'Bottleneck~=1.3.2; python_version<"3.9"',
         'Bottleneck~=1.4.0; python_version>="3.9"',
-        'python-json-logger~=2.0.0',
+        "python-json-logger~=2.0",  # Avoid breaking change in 3.1.0 https://github.com/nhairs/python-json-logger/issues/29
         'jep==4.1.1',
         'kafka-python==1.4.6',
         'deprecated>=1.2.12',
