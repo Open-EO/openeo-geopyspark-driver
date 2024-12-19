fe5e96325bf3e88e417f406302ce7a373f3982cb
Stefaan Lippens <stefaan.lippens@vito.be>
2024-12-19 11:09:41 +0100

    Issue #936 fixup calrissian tests

--- tests/integrations/test_calrissian.py
+++ tests/integrations/test_calrissian.py
@@ -1,104 +1,151 @@
 import dirty_equals
-from openeogeotrellis.integrations.calrissian import create_cwl_job_body
+import kubernetes.client
+
+from openeogeotrellis.integrations.calrissian import CalrissianJobLauncher, CalrissianS3Result
 
 
-def test_create_cwl_job_body():
-    body = create_cwl_job_body(namespace="calrissian-test")
+class TestCalrissianJobLauncher:
 
-    assert body.to_dict() == {
-        "api_version": None,
-        "kind": None,
-        "metadata": dirty_equals.IsPartialDict(
+    def test_create_input_staging_job_manifest(self):
+        launcher = CalrissianJobLauncher(namespace="calrissian-test", name_base="r-123")
+
+        manifest, cwl_path = launcher.create_input_staging_job_manifest(cwl_content="class: Dummy")
+
+        assert cwl_path == "/calrissian/input-data/r-123.cwl"
+
+        assert isinstance(manifest, kubernetes.client.V1Job)
+        manifest_dict = manifest.to_dict()
+
+        assert manifest_dict["metadata"] == dirty_equals.IsPartialDict(
             {
-                "name": dirty_equals.IsStr(regex="cj-2.*"),
+                "name": "r-123-cal-input",
                 "namespace": "calrissian-test",
             }
-        ),
-        "spec": dirty_equals.IsPartialDict(
+        )
+        assert manifest_dict["spec"] == dirty_equals.IsPartialDict(
             {
-                "template": dirty_equals.IsPartialDict(
-                    {
-                        "spec": dirty_equals.IsPartialDict(
-                            {
-                                "containers": [
-                                    dirty_equals.IsPartialDict(
-                                        {
-                                            "args": [
-                                                "--max-ram",
-                                                "2G",
-                                                "--max-cores",
-                                                "1",
-                                                "--debug",
-                                                "--tmp-outdir-prefix",
-                                                "/calrissian/tmpout/",
-                                                "--outdir",
-                                                "/calrissian/output-data/",
-                                                "/calrissian/input-data/hello-workflow.cwl",
-                                                "/calrissian/input-data/hello-input.yaml",
-                                            ],
-                                            "command": ["calrissian"],
-                                            "image": "ghcr.io/duke-gcb/calrissian/calrissian:0.17.1",
-                                            "volume_mounts": [
-                                                dirty_equals.IsPartialDict(
-                                                    {
-                                                        "mount_path": "/calrissian/input-data",
-                                                        "name": "calrissian-input-data",
-                                                        "read_only": True,
-                                                    }
-                                                ),
-                                                dirty_equals.IsPartialDict(
-                                                    {
-                                                        "mount_path": "/calrissian/tmpout",
-                                                        "name": "calrissian-tmpout",
-                                                        "read_only": None,
-                                                    }
-                                                ),
-                                                dirty_equals.IsPartialDict(
-                                                    {
-                                                        "mount_path": "/calrissian/output-data",
-                                                        "name": "calrissian-output-data",
-                                                        "read_only": None,
-                                                    }
-                                                ),
-                                            ],
-                                            "working_dir": None,
-                                        }
-                                    )
-                                ],
-                                "volumes": [
-                                    dirty_equals.IsPartialDict(
-                                        {
-                                            "name": "calrissian-input-data",
-                                            "persistent_volume_claim": {
-                                                "claim_name": "calrissian-input-data",
-                                                "read_only": True,
-                                            },
-                                        }
-                                    ),
-                                    dirty_equals.IsPartialDict(
-                                        {
-                                            "name": "calrissian-tmpout",
-                                            "persistent_volume_claim": {
-                                                "claim_name": "calrissian-tmpout",
-                                                "read_only": None,
-                                            },
-                                        }
-                                    ),
-                                    dirty_equals.IsPartialDict(
-                                        {
-                                            "name": "calrissian-output-data",
-                                            "persistent_volume_claim": {
-                                                "claim_name": "calrissian-output-data",
-                                                "read_only": None,
-                                            },
-                                        }
-                                    ),
-                                ],
-                            }
-                        ),
-                    }
-                ),
+                "backoff_limit": 1,
             }
-        ),
-        "status": None,
-    }
+        )
+        assert manifest_dict["spec"]["template"]["spec"] == dirty_equals.IsPartialDict(
+            {
+                "containers": [
+                    dirty_equals.IsPartialDict(
+                        {
+                            "name": "calrissian-input-staging",
+                            "image": "alpine:3",
+                            "command": ["/bin/sh"],
+                            "args": [
+                                "-c",
+                                "set -euxo pipefail; echo 'Y2xhc3M6IER1bW15' | base64 -d > /calrissian/input-data/r-123.cwl",
+                            ],
+                            "volume_mounts": [
+                                dirty_equals.IsPartialDict(
+                                    {
+                                        "mount_path": "/calrissian/input-data",
+                                        "name": "calrissian-input-data",
+                                        "read_only": False,
+                                    }
+                                ),
+                            ],
+                        }
+                    )
+                ],
+                "volumes": [
+                    dirty_equals.IsPartialDict(
+                        {
+                            "name": "calrissian-input-data",
+                            "persistent_volume_claim": {"claim_name": "calrissian-input-data", "read_only": False},
+                        }
+                    ),
+                ],
+            }
+        )
+
+    def test_create_cwl_job_manifest(self):
+        launcher = CalrissianJobLauncher(namespace="calrissian-test", name_base="r-123")
+
+        manifest, output_dir = launcher.create_cwl_job_manifest(
+            cwl_path="/calrissian/input-data/r-123.cwl", cwl_arguments=["--message", "Howdy Earth!"]
+        )
+
+        assert output_dir == "r-123-cal-cwl"
+
+        assert isinstance(manifest, kubernetes.client.V1Job)
+        manifest_dict = manifest.to_dict()
+
+        assert manifest_dict["metadata"] == dirty_equals.IsPartialDict(
+            {
+                "name": "r-123-cal-cwl",
+                "namespace": "calrissian-test",
+            }
+        )
+        assert manifest_dict["spec"] == dirty_equals.IsPartialDict(
+            {
+                "backoff_limit": 1,
+            }
+        )
+        assert manifest_dict["spec"]["template"]["spec"] == dirty_equals.IsPartialDict(
+            {
+                "containers": [
+                    dirty_equals.IsPartialDict(
+                        {
+                            "name": "r-123-cal-cwl",
+                            "command": ["calrissian"],
+                            "args": dirty_equals.Contains(
+                                "--tmp-outdir-prefix",
+                                "/calrissian/tmpout/",
+                                "--outdir",
+                                "/calrissian/output-data/r-123-cal-cwl",
+                                "/calrissian/input-data/r-123.cwl",
+                                "--message",
+                                "Howdy Earth!",
+                            ),
+                            "volume_mounts": [
+                                dirty_equals.IsPartialDict(
+                                    {
+                                        "mount_path": "/calrissian/input-data",
+                                        "name": "calrissian-input-data",
+                                        "read_only": True,
+                                    }
+                                ),
+                                dirty_equals.IsPartialDict(
+                                    {
+                                        "mount_path": "/calrissian/tmpout",
+                                        "name": "calrissian-tmpout",
+                                        "read_only": False,
+                                    }
+                                ),
+                                dirty_equals.IsPartialDict(
+                                    {
+                                        "mount_path": "/calrissian/output-data",
+                                        "name": "calrissian-output-data",
+                                        "read_only": False,
+                                    }
+                                ),
+                            ],
+                        }
+                    )
+                ],
+                "volumes": [
+                    dirty_equals.IsPartialDict(
+                        {
+                            "name": "calrissian-input-data",
+                            "persistent_volume_claim": {"claim_name": "calrissian-input-data", "read_only": True},
+                        }
+                    ),
+                    dirty_equals.IsPartialDict(
+                        {
+                            "name": "calrissian-tmpout",
+                            "persistent_volume_claim": {"claim_name": "calrissian-tmpout", "read_only": False},
+                        }
+                    ),
+                    dirty_equals.IsPartialDict(
+                        {
+                            "name": "calrissian-output-data",
+                            "persistent_volume_claim": {"claim_name": "calrissian-output-data", "read_only": False},
+                        }
+                    ),
+                ],
+            }
+        )
