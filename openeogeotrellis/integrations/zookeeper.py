06f2057a31f38cd514eb3952537895b496681f34
Stefaan Lippens <stefaan.lippens@vito.be>
2025-02-04 14:42:58 +0100

    PR #992 some fine-tuning

--- openeogeotrellis/integrations/zookeeper.py
+++ openeogeotrellis/integrations/zookeeper.py
@@ -1,18 +1,11 @@
 from __future__ import annotations
 
+import logging
 from time import sleep
-from typing import TYPE_CHECKING
+from typing import Optional
 
-if TYPE_CHECKING:
-    from typing import Union, Any, Callable, Optional
-    from kazoo.protocol.states import ZnodeStat
-
-    # Helper type which corresponds to what Zookeeper for callbacks as used by watches
-    ZEvent = tuple[bytes, ZnodeStat]
-    ZCallback = Callable[[ZEvent], None]
-
-from kazoo.client import KazooClient
 import kazoo.exceptions
+from kazoo.client import KazooClient
 from kazoo.retry import KazooRetry
 from reretry import retry
 
@@ -23,7 +16,8 @@ class ZookeeperClient:
     def __init__(
         self,
         hosts: str,
-        logger: Any = None,
+        *,
+        logger: Optional[logging.Logger] = None,
         attempt_timeout: float = 1.0,
         total_timeout: float = 120.0,
         tries: int = 10,
@@ -35,33 +29,30 @@ class ZookeeperClient:
         multiple zookeeper requests are to be made as part of an application lifetime.
 
         The resulting object is to be used similar as the KazooClient:
-          > ZookeeperClient("zk1.local:2181,zk2.local:2181").get("/path/to/znode")
 
-        attempt_timeout is how much time budget there is to perform 1 attempt at performing an operation (e.g. creating
-        a connection). The default of 1 second is even sufficient when working over VPN and port-forwarding.
+            >>> ZookeeperClient("zk1.local:2181,zk2.local:2181").get("/path/to/znode")
 
-        total_timeout is the total time budget when things go wrong to get a working connection against the cluster.
-        This is in seconds and the default budget is 2 minutes.
+        :param attempt_timeout: is how much time budget there is to perform 1 attempt at performing an operation
+            (e.g. creating a connection). The default of 1 second is even sufficient when working over VPN and port-forwarding.
+        :param total_timeout: is the total time budget when things go wrong to get a working connection against the cluster.
+            This is in seconds and the default budget is 2 minutes.
+        :param tries: is the total amount of connection attempts that one should try in case there are issues.
+        :param delay: is how long to wait in seconds before the next attempt
+        :param backoff: is a multiplier for subsequent delays
 
-        tries is the total amount of connection attempts that one should try in case there are issues.
+        Note that the defaults will allow for roughly 10 attempts in 2 minutes spread in time.
 
-        delay is how long to wait in seconds before the next attempt
-
-        backoff is a multiplier for subsequent delays
-
-        Note that the default will allow for 10 attempts in 2 minutes spread in time.
-
-        attempt delay_sec total_time(this attempt)	total_time(sum)
-        1       0.4	           1.4	                    1.4
-        2     	0.68	       1.68	                    3.08
-        3   	1.156	       2.156	                5.236
-        4       1.9652	       2.9652	                8.2012
-        5   	3.34084	       4.34084	               12.54204
-        6    	5.679428	   6.679428	               19.221468
-        7    	9.6550276	  10.6550276	           29.8764956
-        8      16.4135469	  17.41354692	           47.29004252
-        9      27.9030297	  28.903029764	           76.193072284
-        10     47.4351505	  48.4351505988	          124.6282228828
+            #   delay (s)   attempt total (s)  cumulative (s)
+            1     0.40           1.40             1.40
+            2     0.68           1.68             3.08
+            3     1.15           2.15             5.23
+            4     1.96           2.96             8.20
+            5     3.34           4.34            12.54
+            6     5.67           6.67            19.22
+            7     9.65          10.65            29.87
+            8    16.41          17.41            47.29
+            9    27.90          28.90            76.19
+            10   47.43          48.43           124.62
 
         """
         kz_retry = KazooRetry(max_tries=tries, delay=delay, backoff=backoff)
@@ -139,7 +130,7 @@ class ZookeeperClient:
 
         def exception_handler(e: Exception) -> None:
             if self._logger is not None:
-                self._logger.warning(f"ZookeeperClient Retry of {method} because of {type(e)}({e})")
+                self._logger.warning(f"ZookeeperClient Retry of {method} because of {e!r}")
             # KazooClient recovers automagically we can just await recovery
             self._wait_up_to_1minute_for_zookeeper_connection()
 
