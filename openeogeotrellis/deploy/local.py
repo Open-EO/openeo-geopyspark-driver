e7f38b2db2be016fb9613fa2eea447e395549499
Emile Sonneveld <emile.sonneveld@vito.be>
2025-02-06 02:09:47 +0100

    _cwl_insar and varia. https://github.com/Open-EO/openeo-geopyspark-driver/issues/1034

--- openeogeotrellis/deploy/local.py
+++ openeogeotrellis/deploy/local.py
@@ -14,6 +14,7 @@ from openeo_driver.util.logging import get_logging_config, setup_logging, show_l
 from openeo_driver.utils import smart_bool
 from openeo_driver.views import build_app
 from openeogeotrellis.config import get_backend_config
+from . import kube
 
 _log = logging.getLogger(__name__)
 
