f1104bff0249122a61c52543e178aee614cee197
Stefaan Lippens <stefaan.lippens@vito.be>
2025-02-17 16:37:10 +0100

    fixup! Issue #909 Deprecate non-standard "logging-threshold" job option

--- tests/test_views.py
+++ tests/test_views.py
@@ -530,9 +530,16 @@ class TestBatchJobs:
             assert meta_data["user_id"] == TEST_USER
             assert meta_data["status"] == "queued"
             assert meta_data["api_version"] == api.api_version
-            assert json.loads(meta_data["specification"]) == (
-                data['process'] if api.api_version_compare.at_least("1.0.0")
-                else {"process_graph": data["process_graph"]})
+            assert json.loads(meta_data["specification"]) == {
+                "process_graph": {
+                    "loadcollection1": {
+                        "process_id": "load_collection",
+                        "arguments": {"id": "BIOPAR_FAPAR_V1_GLOBAL"},
+                        "result": True,
+                    }
+                },
+                "job_options": {"log_level": "info"},
+            }
             assert meta_data["application_id"] == 'application_1587387643572_0842'
             assert meta_data["created"] == "2020-04-20T16:04:03Z"
             res = api.get('/jobs/{j}'.format(j=job_id), headers=TEST_USER_AUTH_HEADER).assert_status_code(200).json
