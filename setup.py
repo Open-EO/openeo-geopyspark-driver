1c7050b87451c0bea4d5471bcacfefa2948c2027
Peter Van Bouwel <peter.vanbouwel@vito.be>
2025-03-28 16:09:50 +0100

    feature: Allow S3 presigned urls against S3 proxy
    If the geopyspark-driver has config set to be able to issue IDP tokens and the STS endpoint as well as a new config `s3_region_proxy_endpoints` which has the S3 proxy endpoint per region then it will generate presigned S3 urls for assets as long as it knows about the asset buckets via a workspace.

--- setup.py
+++ setup.py
@@ -33,6 +33,11 @@ tests_require = [
     "pydantic~=1.0",
 ]
 
+typing_require = [
+    'mypy-boto3-sts',
+    'mypy-boto3-s3',
+]
+
 setup(
     name='openeo-geopyspark',
     version=version,
@@ -107,7 +112,7 @@ setup(
         "importlib_resources; python_version<'3.9'",  # #1060 on python 3.8 we need importlib_resources backport
     ],
     extras_require={
-        "dev": tests_require,
+        "dev": tests_require + typing_require,
         "k8s": [
             "kubernetes",
             "PyYAML",
