09cfb790ded080b38184954a0725c04d93ee7321
Thomas Cassaert <tcassaert@inuits.eu>
2025-06-02 11:48:15 +0200

    Remove TODO comment as this fixes the TODO
    Signed-off-by: Thomas Cassaert <tcassaert@inuits.eu>

--- setup.py
+++ setup.py
@@ -64,8 +64,6 @@ setup(
         "openeo_driver>=0.135.0a2.dev",
         'pyspark==3.5.3; python_version>="3.8"',
         'pyspark>=2.3.1,<2.4.0; python_version<"3.8"',
-        # TODO #1220 the +openeo suffix to point to this fork of geopyspark seems to cause trouble in some newer build contexts
-        #      move to a more explicit forked project, e.g. `geopyspark-openeo`?
         'geopyspark_openeo==0.4.3.post1',
         # rasterio is an undeclared but required dependency for geopyspark
         # (see https://github.com/locationtech-labs/geopyspark/issues/683 https://github.com/locationtech-labs/geopyspark/pull/706)
