9ebfdb9763f13016a413b67a304d205b76a50a40
pvbouwel <463976+pvbouwel@users.noreply.github.com>
2025-08-07 10:02:12 +0200

    Update openeogeotrellis/job_options.py
    Co-authored-by: Stefaan Lippens <soxofaan@users.noreply.github.com>

--- openeogeotrellis/job_options.py
+++ openeogeotrellis/job_options.py
@@ -55,7 +55,7 @@ class JobOptions:
         default=get_backend_config().default_python_memory,
         metadata={
             "description": "Setting to specifically limit the memory used by python on a worker. "
-            "Leaving this setting empty will enforce the default value of the backend. None will be zero bytes."
+            "Leaving this setting empty will enforce the default value of the backend. None will be zero bytes. "
             "Typical processes that use python-memory are UDF's, sar_backscatter or Sentinel 3 data loading. "
             "This memory not a reservation so it can act as executor_memory_overhead but it is enforced as a limit."
             "Memory allocation problems with one of aforementioned processes likely warrant an increase of this value."
