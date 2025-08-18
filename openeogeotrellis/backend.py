8af41d85038a2ce2ca415db0b3a21d6100a5d739
Jeroen Dries <jeroen.dries@vito.be>
2025-08-18 08:36:55 +0200

    extra checking on image name https://github.com/eu-cdse/openeo-cdse-infra/issues/515

--- openeogeotrellis/backend.py
+++ openeogeotrellis/backend.py
@@ -1960,7 +1960,12 @@ class GpsBatchJobs(backend.BatchJobs):
             running_image = api_instance_core.read_namespaced_pod(name=os.environ.get("POD_NAME"), namespace=os.environ.get("POD_NAMESPACE")).spec.containers[0].image
 
             image_name = job_options.get("image-name", running_image)
-            image_name = get_backend_config().batch_runtime_to_image.get(image_name.lower(), image_name)
+            if "image-name" in job_options and image_name.lower() not in get_backend_config().batch_runtime_to_image:
+                if re.compile(get_backend_config().batch_image_regex).fullmatch(
+                        image_name) is None:  # validate image name
+                    raise OpenEOApiException(f"invalid value {image_name} for job_option image-name")
+            else:
+                image_name = get_backend_config().batch_runtime_to_image.get(image_name.lower(), image_name)
 
             batch_job_cfg_secret_name = k8s_get_batch_job_cfg_secret_name(spark_app_id)
 
