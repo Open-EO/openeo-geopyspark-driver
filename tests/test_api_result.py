fc4e5423469dbb0937849b100191bbd5bb025f98
Emile Sonneveld <emile.sonneveld@vito.be>
2025-09-08 22:45:48 +0200

    @pytest.mark.timeout(120)

--- tests/test_api_result.py
+++ tests/test_api_result.py
@@ -4135,7 +4135,7 @@ class TestLoadStac:
         parsed = pandas.read_csv(io.StringIO(res.text))
         print(parsed)
 
-    @pytest.mark.skip(reason="Timeout?")
+    @pytest.mark.timeout(120)
     def test_load_stac_from_spatiotemporal_netcdf_mixed_columns_error(self, api110, tmp_path):
         """
         Request with the same order as in the stac catalog will throw an error if it is not alphabetical
@@ -4213,6 +4213,7 @@ class TestLoadStac:
             "Band order should be alphabetical for NetCDF STAC-catalog with a time dimension." in exc_info.value.args[0]
         )
 
+    @pytest.mark.timeout(120)
     @pytest.mark.parametrize(
         "save_format",
         [
