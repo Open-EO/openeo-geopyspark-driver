aae7b3375f3a6bae2a849470cc77b2046d6dfdd5
dsamaey <diether.samaey@vito.be>
2025-09-30 12:51:36 +0200

    chore: add test for slope process

--- tests/test_slope.py
+++ tests/test_slope.py
@@ -11,7 +11,17 @@ from .data import get_test_data_file
 
 def test_slope(imagecollection_with_two_bands_and_three_dates):
     result = imagecollection_with_two_bands_and_three_dates.slope()
+    print("##### result.metadata")
     print(result.metadata)
+    print("##### result")
     print(result)
+
+    the_date = datetime.datetime(2017, 9, 25, 11, 37)
+    result_xarray = result._to_xarray()
+    slope_band = result_xarray.sel(bands='slope', t=the_date)
+    print("##### slope_band")
+    print(slope_band)
+
+
 
 
