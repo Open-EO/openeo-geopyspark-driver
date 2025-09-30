a0dfb907d8555bb86391c3e22638244ba83dc776
dsamaey <diether.samaey@vito.be>
2025-09-30 11:12:44 +0200

    chore: add test for slope process

--- tests/test_slope.py
+++ tests/test_slope.py
@@ -12,5 +12,6 @@ from .data import get_test_data_file
 def test_slope(imagecollection_with_two_bands_and_three_dates):
     result = imagecollection_with_two_bands_and_three_dates.slope()
     print(result.metadata)
+    print(result)
 
 
