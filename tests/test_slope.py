c597652102864cfba713d6dd30a59c5b8676e4c3
dsamaey <diether.samaey@vito.be>
2025-09-30 17:40:02 +0200

    chore: add test for slope process

--- tests/test_slope.py
+++ tests/test_slope.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 import numpy as np
 import pytest
 from geopyspark import SpaceTimeKey, Tile
+from numpy import ndarray
 from numpy.testing import assert_array_almost_equal
 from openeo_driver.utils import EvalEnv
 
@@ -38,6 +39,15 @@ def test_slope(imagecollection_with_two_bands_and_three_dates):
     nir_band = input_xarray.sel(bands='nir', t=the_date)
     logger.info("##### input nir band")
     logger.info(nir_band)
-    slope_band = result_xarray.sel(bands='slope', t=the_date)
-    logger.info("##### result slope band")
-    logger.info(slope_band)
+    t1_slope_band = result_xarray.sel(bands='slope', t=the_date)
+    logger.info("##### result t1 slope band")
+    t1_slope_values: ndarray = t1_slope_band.values
+    logger.info(t1_slope_values)
+    assert t1_slope_values[0, 0] == 0
+    assert t1_slope_values[31,31] == np.NaN
+    t2_slope_band = result_xarray.sel(bands='slope', t=the_date)
+    logger.info("##### result t2 slope band")
+    t2_slope_values: ndarray = t2_slope_band.values
+    logger.info(t2_slope_values)
+    assert t2_slope_values[0, 0] == np.NaN
+    assert t2_slope_values[31,31] == np.NaN
