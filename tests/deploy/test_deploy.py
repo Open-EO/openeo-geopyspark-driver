ac6de1b0adac6c716f63e543be91a2032722bb70
Stefaan Lippens <stefaan.lippens@vito.be>
2025-10-08 11:47:40 +0200

    more lenient tests about jar versions
    our jars don't always follow semver (e.g. feature branch builds), so be more flexible about that
    
    refs Open-EO/openeo-geopyspark-driver#1377, eu-cdse/openeo-cdse-infra#742

--- tests/deploy/test_deploy.py
+++ tests/deploy/test_deploy.py
@@ -156,12 +156,16 @@ def test_build_gps_backend_deploy_metadata():
         packages=["openeo", "openeo_driver", "openeo-geopyspark", "geopyspark"],
         jar_paths=JAR_DIR.glob("geotrellis-*.jar")
     )
-    assert metadata == DictSubSet({
-        "versions": DictSubSet({
-            "openeo": RegexMatcher("\d+.\d+.\d+"),
-            "geotrellis-extensions": RegexMatcher(r"\d+\.\d+.\d+_\d+\.\d+"),
-        })
-    })
+    assert metadata == DictSubSet(
+        {
+            "versions": DictSubSet(
+                {
+                    "openeo": RegexMatcher("\d+.\d+.\d+"),
+                    "geotrellis-extensions": dirty_equals.IsStr(regex=r"\d+\.\d+.\d+_\d+\.\d+|PR-\d+.*"),
+                }
+            )
+        }
+    )
 
 
 def test_find_geotrellis_jars_cwd(tmp_path, monkeypatch):
