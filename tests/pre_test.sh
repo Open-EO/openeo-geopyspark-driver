949ebab324c75b863e7e729a50f086e886370df2
Stefaan Lippens <stefaan.lippens@vito.be>
2025-10-08 14:34:52 +0200

    Add bit of context and documentation to pre_test.sh

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+# pre-test procedure specific for VITO Jenkins CI toolchain
+# executed just before running the tests
+
 set -eux
 pwd
 
+# TODO: is this `/eodata` still necessary or can this be eliminated?
 mkdir /eodata
 chown jenkins /eodata
 
@@ -11,5 +15,7 @@ source ./env.sh
 python3 scripts/get-jars.py --force-download --python-version ${PYTHON_VERSION:-3.8} jars
 '
 
+# Root folder for temp folders during tests (e.g. through `tmp_path` fixture).
+# Assigned to `PYTEST_DEBUG_TEMPROOT` from Jenkinsfile.
 mkdir -p pytest-tmp
 chown jenkins pytest-tmp
