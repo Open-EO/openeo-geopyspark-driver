a855477d0e96a0e17f48374176a124f9acb21af1
Stefaan Lippens <stefaan.lippens@vito.be>
2025-10-14 15:44:50 +0200

    dump_library_versions utility: support compact json output format
    and sort listing for consistency

--- openeogeotrellis/udf/dump_library_versions.py
+++ openeogeotrellis/udf/dump_library_versions.py
@@ -1,17 +1,21 @@
 """
 Simple CLI utility to dump the versions of provided libraries
 (as available in current environment)
-in a format that's compatible with ContainerImageRecord
+in a format that's compatible with `ContainerImageRecord.parse_udf_runtime_libraries`
 """
 
 import argparse
-import logging
 import json
+import logging
 
 from openeo_driver.utils import get_package_version
 
-
 _log = logging.getLogger(__name__)
+
+
+class OUTPUT_FORMAT:
+    JSON_VERBOSE = "json-verbose"
+    JSON_ONELINE = "json-compact"
 
 
 def main(argv=None) -> None:
@@ -22,9 +26,15 @@ def main(argv=None) -> None:
         default=["openeo", "numpy", "xarray"],
         help="Libraries to dump version for. Can be given as separate arguments or comma-separated.",
     )
-    args = cli.parse_args(argv)
+    cli.add_argument(
+        "--format",
+        choices=[OUTPUT_FORMAT.JSON_VERBOSE, OUTPUT_FORMAT.JSON_ONELINE],
+        default=OUTPUT_FORMAT.JSON_VERBOSE,
+        help="Output format (default: %(default)r)",
+    )
+    cli_args = cli.parse_args(argv)
 
-    libraries = {lib.strip() for libs in args.libraries for lib in libs.split(",")}
+    libraries = sorted({lib.strip() for libs in cli_args.libraries for lib in libs.split(",")})
     _log.info(f"Looking up versions of {libraries=}")
 
     data = {}
@@ -36,7 +46,15 @@ def main(argv=None) -> None:
             _log.warning(f"No version found for {lib!r}")
 
     # TODO: options for YAML or single-line JSON output?
-    print(json.dumps(data, indent=2))
+
+    if cli_args.format == OUTPUT_FORMAT.JSON_ONELINE:
+        json_kwargs = {"separators": (",", ":")}
+    elif cli_args.format == OUTPUT_FORMAT.JSON_VERBOSE:
+        json_kwargs = {"indent": 2}
+    else:
+        raise ValueError(cli_args.format)
+
+    print(json.dumps(data, **json_kwargs))
 
 
 if __name__ == "__main__":
