e8f78b418a6de5628151fd8c5b647480482620fd
Emile Sonneveld <emile.sonneveld@vito.be>
2025-12-02 14:54:39 +0100

    4h timeout. https://github.com/Open-EO/openeo-geopyspark-driver/issues/1439

--- openeogeotrellis/integrations/s3proxy/sts.py
+++ openeogeotrellis/integrations/s3proxy/sts.py
@@ -82,7 +82,7 @@ def _get_aws_credentials_for_proxy(token: str, role_arn: str, session_name: Opti
             RoleArn=role_arn,
             RoleSessionName=session_name,
             WebIdentityToken=token,
-            DurationSeconds=7200,  # same timeout as in calrissian launch_job_and_wait.
+            DurationSeconds=14400,  # Longer timeout as in calrissian launch_job_and_wait.
         )["Credentials"]
     )
 
