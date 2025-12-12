873968267dd880a966711e8fb0dc87f0edf3af68
Stefaan Lippens <stefaan.lippens@vito.be>
2025-12-13 00:10:37 +0100

    fixup test_stac_api_no_spatial_extent_specified
    start using real assets from dummy_stac_api_server

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     tests_require=tests_require,
     install_requires=[
         # TODO: avoid excessive `python_version` based version constraints. https://github.com/Open-EO/openeo-geopyspark-driver/issues/1395
-        "openeo>=0.43.0.dev",
+        "openeo>=0.47.0.a4.dev",
         "openeo_driver>=0.138.0a7.dev",
         'pyspark>=4.0.0; python_version>"3.8"',
         'pyspark>=3.5.0,<4.0.0; python_version<="3.8"',
