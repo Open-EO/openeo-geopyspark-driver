2f4c2b00c04f3c22f7dfa54deeb02ad104309352
Jan Van den bosch <jan@bossie.org>
2026-01-22 16:44:40 +0100

    fixup! adapt to latest pystac (currently 1.14.3) #1496

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         'kafka-python==1.4.6',
         'deprecated>=1.2.12',
         'elasticsearch==7.16.3',
-        "pystac>=1.8.4",
+        "pystac>=1.14.3",
         'pystac_client~=0.7.2',
         'boto3>=1.16.25,<2.0',
         "hvac>=1.0.2",
