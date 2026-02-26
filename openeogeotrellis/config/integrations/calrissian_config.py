d4ad7635358a78d7773337c95ef5b0a28d074649
Emile Sonneveld <emile.sonneveld@vito.be>
2026-02-26 10:47:05 +0100

    Remove TODO.

--- openeogeotrellis/config/integrations/calrissian_config.py
+++ openeogeotrellis/config/integrations/calrissian_config.py
@@ -19,7 +19,7 @@ DEFAULT_CALRISSIAN_S3_BUCKET = "calrissian"
 DEFAULT_CALRISSIAN_BASE_ARGUMENTS: tuple = (
     "--debug",
     "--max-ram",
-    "128G",  # Seems to be a limit for pods combined across nodes. Testing with higher threshold. # TODO: Use max_executor_or_driver_memory ?
+    "128G",  # Seems to be a limit for pods combined across nodes. Testing with higher threshold.
     "--max-cores",
     "1000",
     "--force-docker-pull",
