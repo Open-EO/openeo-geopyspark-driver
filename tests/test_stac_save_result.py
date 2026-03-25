8e0866cd299e422b05645c6419f11ff0886c745f
Emile Sonneveld <emile.sonneveld@vito.be>
2026-03-25 11:10:10 +0100

    Move some stac functions to openeo-python-driver. https://github.com/cloudinsar/s1-workflows/issues/80

--- tests/test_stac_save_result.py
+++ tests/test_stac_save_result.py
@@ -5,64 +5,10 @@ from pystac import Collection
 
 from openeogeotrellis.stac_save_result import (
     StacSaveResult,
-    get_files_from_stac_catalog,
-    get_assets_from_stac_catalog,
-    get_items_from_stac_catalog,
 )
 
 repository_root = Path(__file__).parent.parent
 print(repository_root)
-
-
-def test_get_files_from_stac_catalog_path():
-    stac_root = repository_root / "docker/local_batch_job/example_stac_catalog/collection.json"
-    ret = get_files_from_stac_catalog(stac_root)
-    print(ret)
-    assert len(ret) == 3
-
-
-def test_get_files_from_stac_catalog_path_include_metadata():
-    stac_root = repository_root / "docker/local_batch_job/example_stac_catalog/collection.json"
-    ret = get_files_from_stac_catalog(stac_root, include_metadata=True)
-    print(ret)
-    assert len(ret) == 7
-
-
-def test_get_files_from_stac_catalog_url():
-    stac_root = "https://raw.githubusercontent.com/Open-EO/openeo-geopyspark-driver/refs/heads/master/docker/local_batch_job/example_stac_catalog/collection.json"
-    ret = get_files_from_stac_catalog(stac_root)
-
-    print(ret)
-    assert len(ret) == 3
-
-
-def test_get_files_from_stac_catalog_url_include_metadata():
-    stac_root = "https://raw.githubusercontent.com/Open-EO/openeo-geopyspark-driver/refs/heads/master/docker/local_batch_job/example_stac_catalog/collection.json"
-    ret = get_files_from_stac_catalog(stac_root, include_metadata=True)
-
-    print(ret)
-    assert len(ret) == 7
-
-
-def test_get_assets_from_stac_catalog():
-    stac_root = repository_root / "docker/local_batch_job/example_stac_catalog/collection.json"
-    ret = get_assets_from_stac_catalog(stac_root)
-    print(ret)
-    assert len(ret.values()) == 3
-
-
-def test_get_items_from_stac_catalog():
-    stac_root = repository_root / "docker/local_batch_job/example_stac_catalog/collection.json"
-    ret = get_items_from_stac_catalog(stac_root)
-    print(ret)
-    assert len(ret) == 3
-
-
-def test_get_items_from_stac_catalog_recursive():
-    stac_root = str(repository_root / "tests/data/stac/recursive-stac-example/collection.json")
-    ret = get_items_from_stac_catalog(stac_root, make_hrefs_absolute=True)
-    print(ret)
-    assert len(ret) == 3
 
 
 def test_stac_save_result():
