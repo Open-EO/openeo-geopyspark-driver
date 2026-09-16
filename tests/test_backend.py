c96ebdbdf7fd6e015394ea902201bb357fb87372
dsamaey <diether.samaey@vito.be>
2026-09-16 19:48:49 +0200

    chore: fix broken test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -891,8 +891,8 @@ def test_k8s_sparkapplication_dict_custom_open_telemetry_prometheus_port(backend
     )
 
     assert app_dict["spec"]["executor"]["ports"] == [
-        {"containerPort": 19464, "name": "prom-executor", "protocol": "TCP"},
-        {"containerPort": 9465, "name": "prom-executor", "protocol": "TCP"},
+        {"containerPort": 19464, "name": "prom-executor-scala", "protocol": "TCP"},
+        {"containerPort": 9465, "name": "prom-executor-python", "protocol": "TCP"},
     ]
     assert "-Dotel.exporter.prometheus.port=19464" in app_dict["spec"]["executor"]["javaOptions"]
 
