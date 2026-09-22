6cf1b127ff5b317b7950a8be7ec3a501197470c5
Jeroen Verstraelen <verstraelenjeroen@gmail.com>
2026-09-22 20:40:11 +0200

    catalog: fix dropped default_opensearch_endpoint in CLI/build tooling
    dump_layer_catalog() and _legacy_enrich_collection_metadata() stopped
    passing the configured default_opensearch_endpoint after the catalog
    module split, so collections enriched via these dev/build tools got
    None instead of the backend default. dump_layer_catalog() also lost
    its fallback to get_backend_config().layer_catalog_files when
    --catalog-file isn't given.

--- openeogeotrellis/catalog/files.py
+++ openeogeotrellis/catalog/files.py
@@ -10,6 +10,7 @@ from typing import List, Union
 from openeo.util import TimingLogger
 from openeo_driver.utils import read_json
 
+from openeogeotrellis.config import get_backend_config
 from openeogeotrellis.util.datastructures import dict_merge_recursive
 
 from .common_name import merge_layers_with_common_name
@@ -93,7 +94,11 @@ def dump_layer_catalog():
 
     logging.basicConfig(level=logging.DEBUG if arguments.verbose else logging.DEBUG)
 
-    metadata = load_catalog_files(catalog_files=arguments.catalog_file or [], enrich_metadata=arguments.enrich)
+    metadata = load_catalog_files(
+        catalog_files=arguments.catalog_file or get_backend_config().layer_catalog_files,
+        enrich_metadata=arguments.enrich,
+        default_opensearch_endpoint=get_backend_config().default_opensearch_endpoint,
+    )
     if arguments.container == "list":
         metadata = list(metadata.values())
     json.dump(metadata, fp=sys.stdout, indent=2)
